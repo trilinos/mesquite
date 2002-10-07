7147767fd39d0a9c53d485954c27c2d69743fe79
brewer <brewer>
2002-10-07 17:05:22 +0000

    Adding the Sandia copyright information to Mesquite.hpp.

--- include/Mesquite.hpp
+++ include/Mesquite.hpp
@@ -23,7 +23,15 @@
 #ifdef MESQUITE_USES_TSTT
 #include "TSTT_Base.h"
 #endif
-
+/*! \file Mesquite.hpp
+  \namespace Mesquite
+  
+         Copyright 2002 Sandia Corporation. Under the terms of 
+         Contract DE-AC04-94AL85000, there is a non-exclusive
+         license for use of this work by or on behalf of the U.S.
+         Government. Export of this program may require a license
+         from the United States Government.
+*/
 namespace Mesquite
 {
    
