17a302bb3f56c5abf013e6f70a26c9e1d6b3f2cf
brewer <brewer>
2002-10-30 16:03:08 +0000

    Adding a simple VertexMover to randomize vertex positions.  It is similar to the laplacian smoother, in that it works on a local patch and doesn't use ObjectiveFunction or QualityMetric.  Currently, it does not keep a planar mesh in the plane, though we can put a work-around in for that if it is needed.  The movement of the free vertex is scaled with respect to a user defined percentage and with respect to the size of the patch. (It currently does not have an option to keep the mesh valid, but I will probably add that at some point.)

---
+++ src/QualityImprover/VertexMover/Randomize/Randomize.cpp
@@ -0,0 +1,81 @@
+/*!
+  \file   Randomize.cpp
+  \brief  
+
+  The Randomize Class is the concrete class that randomizes
+  the vertex positions.          
+
+  \author Michael Brewer
+  \date   2002-10-27
+*/
+
+
+#include "Randomize.hpp"
+
+#include "TSTT_C.h"
+
+using namespace Mesquite;
+
+
+#undef __FUNC__
+#define __FUNC__ "Randomize::Randomize" 
+Randomize::Randomize() 
+{
+  this->set_name("Randomize");
+  mPercent=.05;
+}  
+
+#undef __FUNC__
+#define __FUNC__ "Randomize::Randomize" 
+Randomize::Randomize(double percent) 
+{
+  this->set_name("Randomize");
+  mPercent=percent;
+}  
+  
+#undef __FUNC__
+#define __FUNC__ "Randomize::initialize" 
+void Randomize::initialize(PatchData &pd, MsqError &err)
+{
+  this->set_patch_depth(1);
+}
+
+#undef __FUNC__
+#define __FUNC__ "Randomize::initialize_mesh_iteration" 
+void Randomize::initialize_mesh_iteration(PatchData &pd, MsqError &err)
+{
+  //  cout << "- Executing Randomize::iteration_complete()\n";
+}
+
+#undef __FUNC__
+#define __FUNC__ "Randomize::optimize_vertex_position" 
+void Randomize::optimize_vertex_positions(PatchData &pd, 
+                                                MsqError &err)
+{
+    //cout << "- Executing Randomize::optimize_vertex_position()\n";
+
+  int num_local_vertices = pd.num_vertices();
+    //int dim = pd.space_dim();
+  int dim = get_mesh_set()->space_dim();  
+  // gets the array of coordinates for the patch and print it 
+  MsqVertex *patch_coords = pd.get_vertex_array(err); MSQ_CHKERR(err);
+  // does the randomize smooth
+  randomize_vertex(num_local_vertices-1, &patch_coords[1],
+                   patch_coords[0], dim, err); MSQ_CHKERR(err);
+}
+  
+#undef __FUNC__
+#define __FUNC__ "Randomize::terminate_mesh_iteration" 
+void Randomize::terminate_mesh_iteration(PatchData &pd, MsqError &err)
+{
+  //  cout << "- Executing Randomize::iteration_complete()\n";
+}
+  
+#undef __FUNC__
+#define __FUNC__ "Randomize::cleanup" 
+void Randomize::cleanup()
+{
+  //  cout << "- Executing Randomize::iteration_end()\n";
+}
+  
+
---
+++ src/QualityImprover/VertexMover/Randomize/Randomize.hpp
@@ -0,0 +1,96 @@
+/*!
+  \file   Randomize.hpp
+  \brief  
+
+  The Randomize Class implements the Randomize Vertex Mover
+  for a patch with one free vertex. 
+
+  \author Michael Brewer      
+  \date   2002-10-27
+*/
+
+#ifndef Mesquite_Randomize_hpp 
+#define Mesquite_Randomize_hpp
+
+#include "Mesquite.hpp"
+#include "VertexMover.hpp"
+#include "MsqMessage.hpp"
+
+#include "TSTT_C.h"
+
+#include <math.h>
+namespace Mesquite
+{
+
+  /*! \class Randomize
+   \brief Randomly perftubs the (un-culled) vertices.
+  */ 
+  class Randomize : public VertexMover 
+  {
+  public:
+      //!Constructor defaulting mPercent to .05.
+    Randomize();
+      //!Constructor allowing user to set mPercent
+    Randomize(double percent);
+
+  protected:
+    virtual void initialize(PatchData &pd, MsqError &err);
+    virtual void optimize_vertex_positions(PatchData &pd,
+                                         MsqError &err);
+    virtual void initialize_mesh_iteration(PatchData &pd, MsqError &err);
+    virtual void terminate_mesh_iteration(PatchData &pd, MsqError &err);
+    virtual void cleanup();
+  private:
+      //! \param The percentage of the scale factor each vertex will be moved.
+    double mPercent;
+      /*!Function calculates a scale factor for the patch, then moves
+        the incident vertex randomly in each of the three coordinate
+        directions (relative to the scale factor multiplied by mPercent).
+      */
+    void randomize_vertex(int num_incident_vtx,
+                          MsqVertex *incident_vtx,
+                          MsqVertex &free_vtx,
+                          int dimension, MsqError &err);
+  };
+
+  
+#undef __FUNC__
+#define __FUNC__ "randomize_vertex"
+    //!Perturbs the free vertex randomly.
+  inline void Randomize::randomize_vertex(int num_incident_vtx,
+                                          MsqVertex *incident_vtx,
+                                          MsqVertex &free_vtx,
+                                          int dimension, MsqError &err)
+  {
+    int i,j;
+      //a scale w.r.t. the patch size
+    double scale_factor=0.0;
+      //a "random" number between -1 and 1
+    double rand_double=0.0;
+      //a "random" int
+    int rand_int=0;
+    if (num_incident_vtx==0){
+      PRINT_WARNING("WARNING: Number of incident vertex is zero.  Returning.\n");
+      return;
+    }
+    
+    for (i=0;i<num_incident_vtx;++i){
+      scale_factor+=(incident_vtx[i]-free_vtx).length();
+    }
+    scale_factor/=( (double) num_incident_vtx );    
+    for (j=0;j<3;++j){
+      rand_int = rand();
+        //number between 0 and 1000
+      rand_int = rand_int%1000;
+        //number between -1 and 1
+      rand_double = (((double) rand_int)/500.0)-1.0;
+      free_vtx[j] += scale_factor*rand_double*mPercent;
+    }
+    
+    return;
+  }
+
+  
+}
+
+#endif
