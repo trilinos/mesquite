62115fcdf46b1dd1d535eae2c8f9d47aaae3a048
tleurent <tleurent>
2002-11-14 17:06:15 +0000

    Unit tested all the instruction queue manipulation functions.

--- testSuite/unit/InstructionQueueTest.cpp
+++ testSuite/unit/InstructionQueueTest.cpp
@@ -8,7 +8,7 @@
 //    E-MAIL: tleurent@mcs.anl.gov
 //
 // ORIG-DATE: 13-Nov-02 at 18:05:56
-//  LAST-MOD: 13-Nov-02 at 17:31:57 by Thomas Leurent
+//  LAST-MOD: 14-Nov-02 at 10:49:44 by Thomas Leurent
 //
 // DESCRIPTION:
 // ============
@@ -41,11 +41,15 @@ using namespace Mesquite;
 class InstructionQueueTest : public CppUnit::TestFixture
 {
 private:
-  CPPUNIT_TEST_SUITE(InstructionQueueTest);
-  CPPUNIT_TEST (test_add_preconditionner);
-//  CPPUNIT_TEST ();
-  CPPUNIT_TEST_SUITE_END();
-
+   CPPUNIT_TEST_SUITE(InstructionQueueTest);
+   CPPUNIT_TEST (test_add_preconditionner);
+   CPPUNIT_TEST (test_remove_preconditioner);
+   CPPUNIT_TEST (test_insert_preconditioner);
+   CPPUNIT_TEST (test_add_quality_assessor);
+   CPPUNIT_TEST (test_remove_quality_assessor);
+   CPPUNIT_TEST (test_insert_quality_assessor);
+   CPPUNIT_TEST_SUITE_END();
+   
 private:
    QualityAssessor* mQA;
    QualityImprover* mQI;
@@ -82,13 +86,115 @@ public:
      mQueue.clear();
      mQueue.add_preconditioner(mQI, err);
      CPPUNIT_ASSERT(!err.errorOn);
+     err.reset();
      mQueue.set_master_quality_improver(mQI,err);
      CPPUNIT_ASSERT(!err.errorOn);
+     err.reset();
      mQueue.add_preconditioner(mQI, err);
      CPPUNIT_ASSERT_MESSAGE("preconditionner cannot be added after master QI"
                             , err.errorOn);     
   }
 
+   void test_remove_preconditioner()
+   {
+      MsqError err;
+      mQueue.clear();
+      mQueue.add_preconditioner(mQI, err);   // 0
+      mQueue.add_quality_assessor(mQA, err); // 1
+      mQueue.add_preconditioner(mQI, err);   // 2
+      mQueue.set_master_quality_improver(mQI, err);
+      CPPUNIT_ASSERT(!err.errorOn);
+      err.reset();
+      mQueue.remove_preconditioner(2, err);
+      CPPUNIT_ASSERT_MESSAGE("should remove QualityImprover", !err.errorOn);
+      err.reset();
+      mQueue.remove_preconditioner(3, err);
+      CPPUNIT_ASSERT_MESSAGE("should not remove master QualityImprover", err.errorOn);
+      err.reset();
+      mQueue.remove_preconditioner(1, err);
+      CPPUNIT_ASSERT_MESSAGE("should not remove QualityAssessor", err.errorOn);
+      err.reset();
+      mQueue.remove_preconditioner(0, err);
+      CPPUNIT_ASSERT_MESSAGE("should  remove QualityImprover", !err.errorOn);
+      err.reset();
+      mQueue.remove_preconditioner(0, err);
+      CPPUNIT_ASSERT_MESSAGE("should not remove QualityAssessor", err.errorOn);   
+   }
+
+   void test_insert_preconditioner()
+   {
+      MsqError err;
+      mQueue.clear();
+      mQueue.add_preconditioner(mQI, err);   // 0
+      mQueue.add_quality_assessor(mQA, err); // 1
+      mQueue.add_preconditioner(mQI, err);   // 2
+      mQueue.set_master_quality_improver(mQI, err);
+      CPPUNIT_ASSERT(!err.errorOn);
+      err.reset();
+      mQueue.insert_preconditioner(mQI,2, err);
+      CPPUNIT_ASSERT(!err.errorOn);
+      err.reset();
+      mQueue.insert_preconditioner(mQI, 5, err);
+      CPPUNIT_ASSERT_MESSAGE("should not insert after master QualityImprover", err.errorOn);
+      err.reset();
+   }
+
+   void test_add_quality_assessor()
+  {
+     MsqError err;
+     mQueue.clear();
+     mQueue.add_quality_assessor(mQA, err);
+     CPPUNIT_ASSERT(!err.errorOn);
+     err.reset();
+     mQueue.set_master_quality_improver(mQI,err);
+     CPPUNIT_ASSERT(!err.errorOn);
+     err.reset();
+     mQueue.add_quality_assessor(mQA, err);
+     CPPUNIT_ASSERT(!err.errorOn);
+  }
+
+   void test_remove_quality_assessor()
+   {
+      MsqError err;
+      mQueue.clear();
+      mQueue.add_preconditioner(mQI, err);   // 0
+      mQueue.add_quality_assessor(mQA, err); // 1
+      mQueue.add_preconditioner(mQI, err);   // 2
+      mQueue.set_master_quality_improver(mQI, err);
+      CPPUNIT_ASSERT(!err.errorOn);
+      err.reset();
+      mQueue.remove_quality_assessor(2, err);
+      CPPUNIT_ASSERT_MESSAGE("should not remove QualityImprover", err.errorOn);
+      err.reset();
+      mQueue.remove_quality_assessor(3, err);
+      CPPUNIT_ASSERT_MESSAGE("should not remove master QualityImprover", err.errorOn);
+      err.reset();
+      mQueue.remove_quality_assessor(1, err);
+      CPPUNIT_ASSERT_MESSAGE("should remove QualityAssessor", !err.errorOn);
+      err.reset();
+      mQueue.remove_quality_assessor(1, err);
+      CPPUNIT_ASSERT_MESSAGE("should not remove QualityImprover", err.errorOn);
+   }
+
+   void test_insert_quality_assessor()
+   {
+      MsqError err;
+      mQueue.clear();
+      mQueue.add_preconditioner(mQI, err);   // 0
+      mQueue.add_quality_assessor(mQA, err); // 1
+      mQueue.add_preconditioner(mQI, err);   // 2
+      mQueue.set_master_quality_improver(mQI, err);
+      CPPUNIT_ASSERT(!err.errorOn);
+      err.reset();
+      mQueue.insert_quality_assessor(mQA,2, err);
+      CPPUNIT_ASSERT(!err.errorOn);
+      err.reset();
+      mQueue.insert_quality_assessor(mQA, 5, err);
+      CPPUNIT_ASSERT(!err.errorOn);
+      err.reset();
+   }
+
+     
 };
 
 
