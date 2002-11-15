40470888637bf41c0c1e43e930d3d1b2e41bffd9
djmelan <djmelan>
2002-11-15 00:29:50 +0000

    You can now pick which test(s) you run with sample_test. Just list the tests on the command line, as in:
      sample_test Misc

--- testSuite/unit/sample_main.cpp
+++ testSuite/unit/sample_main.cpp
@@ -1,26 +1,40 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include "MesquiteTestRunner.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
     // Create a test runner
   Mesquite::TestRunner runner;
+
+    // If the user requested a specific test...
+  if (argc > 1)
+  {
+    while (argc > 1)
+    {
+      argc--;
+      CppUnit::TestFactoryRegistry &registry =
+        CppUnit::TestFactoryRegistry::getRegistry(argv[argc]);
+      runner.add_test( registry.makeTest() );
+    }
+  }
+  else
+  {
+      // Get the test suites we want to run
+    CppUnit::TestFactoryRegistry &registry =
+      CppUnit::TestFactoryRegistry::getRegistry("Misc");
+    runner.add_test( registry.makeTest() );
+    
+    CppUnit::TestFactoryRegistry &registry2 =
+      CppUnit::TestFactoryRegistry::getRegistry("MsqMeshEntityTest");
+    runner.add_test( registry2.makeTest() );
+    
+    CppUnit::TestFactoryRegistry &registry3 =
+      CppUnit::TestFactoryRegistry::getRegistry("InstructionQueueTest");
+    runner.add_test( registry3.makeTest() );
+  }
   
-    // Get the test suites we want to run
-  CppUnit::TestFactoryRegistry &registry =
-    CppUnit::TestFactoryRegistry::getRegistry("Misc");
-  runner.add_test( registry.makeTest() );
-
-  CppUnit::TestFactoryRegistry &registry2 =
-    CppUnit::TestFactoryRegistry::getRegistry("MsqMeshEntityTest");
-  runner.add_test( registry2.makeTest() );
-
-  CppUnit::TestFactoryRegistry &registry3 =
-    CppUnit::TestFactoryRegistry::getRegistry("InstructionQueueTest");
-  runner.add_test( registry3.makeTest() );
-
     // Run the tests
-  bool wasSucessful = runner.run("Darryl's Test Run");
+  bool wasSucessful = runner.run("Test Run");
   
     // Return 0 if there were no errors
   return wasSucessful ? 0 : 1;
