8aba4ed055c2034cea86b29aea027ded3a863ba2
tleurent <tleurent>
2002-11-20 01:02:48 +0000

    more tests on PatchData. The test test_get_element_vertex_indices generates a segfault. This could be an uncaught bug in MsqMeshEntity. I'll look at it tomorrow morning.

--- testSuite/unit/PatchDataTest.cpp
+++ testSuite/unit/PatchDataTest.cpp
@@ -8,7 +8,7 @@
 //    E-MAIL: tleurent@mcs.anl.gov
 //
 // ORIG-DATE: 13-Nov-02 at 18:05:56
-//  LAST-MOD: 19-Nov-02 at 15:12:42 by Thomas Leurent
+//  LAST-MOD: 19-Nov-02 at 18:51:11 by Thomas Leurent
 //
 // DESCRIPTION:
 // ============
@@ -35,17 +35,14 @@ class PatchDataTest : public CppUnit::TestFixture
 private:
    CPPUNIT_TEST_SUITE(PatchDataTest);
    CPPUNIT_TEST (test_add_vertex);
+   CPPUNIT_TEST (test_reserve_vertex_capacity);
+   CPPUNIT_TEST (test_reserve_element_capacity);
+   CPPUNIT_TEST (test_get_element_vertex_indices);
+   CPPUNIT_TEST (test_get_vertex_element_indices);   
    CPPUNIT_TEST_SUITE_END();
    
 private:
 
-   /* our 2D set up: 2 triangles and one quad are available
-   ---------
-   |\  |   |
-   | \ |   |
-   |  \|   |
-   ---------
-   */
    MsqVertex vtx_0_0;
    MsqVertex vtx_0_1;
    MsqVertex vtx_1_0;
@@ -70,6 +67,13 @@ public:
      vtx_2_0.set(2,0,0);
      vtx_2_1.set(2,1,0);
 
+     /* our 2D set up: 2 triangles and one quad are available
+        
+       1___3___5
+        |\2|   |
+        |1\| 3 |
+       0---2---4
+     */
      mPatch2D.reserve_vertex_capacity(6, err); MSQ_CHKERR(err);
      mPatch2D.add_vertex(NULL, NULL, 0,0,0, true, err); MSQ_CHKERR(err);
      mPatch2D.add_vertex(NULL, NULL, 0,1,0, true, err); MSQ_CHKERR(err);
@@ -96,6 +100,37 @@ public:
   PatchDataTest()
     {}
   
+   void test_reserve_vertex_capacity()
+   {
+      MsqError err;
+
+      // checks that adding too many vertices returns an error.
+      mPatch2D.add_vertex(NULL, NULL, 1.5,5.6,0, true, err);
+      CPPUNIT_ASSERT(err.errorOn);
+      err.reset();
+
+      mPatch2D.add_vertex(NULL, NULL, 1.5,5.6,0, false, err);
+      CPPUNIT_ASSERT(err.errorOn);
+      err.reset();
+
+      int num = mPatch2D.num_vertices();
+      CPPUNIT_ASSERT_EQUAL(6, num);
+   }
+   
+   void test_reserve_element_capacity()
+   {
+      MsqError err;
+
+      int ind[3];
+      ind[0] = 4; ind[1]=2; ind[2]=1;
+      mPatch2D.add_element(NULL, NULL, ind, TRIANGLE, err);
+      CPPUNIT_ASSERT(err.errorOn);
+      err.reset();
+      
+      int num = mPatch2D.num_elements();
+      CPPUNIT_ASSERT_EQUAL(3, num);
+   }
+   
    void test_add_vertex()
    {
       MsqError err;
@@ -108,9 +143,42 @@ public:
       CPPUNIT_ASSERT_EQUAL(vtx_1_1, vertices[3]);
       CPPUNIT_ASSERT_EQUAL(vtx_2_0, vertices[4]);
       CPPUNIT_ASSERT_EQUAL(vtx_2_1, vertices[5]);
+
+      int ind = mPatch2D.add_vertex(NULL, NULL, 0,0,0, true, err);
+      CPPUNIT_ASSERT_EQUAL(0, ind);
+      CPPUNIT_ASSERT_MESSAGE("vertex already existed.", !err.errorOn);
    }
 
-     
+   void test_get_element_vertex_indices() {
+
+      MsqError err;
+      
+      std::vector<size_t> vtx_ind;
+      std::vector<size_t> res;
+      
+      mPatch2D.get_element_vertex_indices(1, vtx_ind, err); MSQ_CHKERR(err);
+      res[0] = 1; res[1] = 2; res[2] = 3;
+      CPPUNIT_ASSERT( vtx_ind==res );
+      mPatch2D.get_element_vertex_indices(2, vtx_ind, err); MSQ_CHKERR(err);
+      res[0] = 3; res[1] = 2; res[2] = 4; res[2] = 5;
+      CPPUNIT_ASSERT( vtx_ind==res );
+   }
+
+   void test_get_vertex_element_indices() {
+      MsqError err;
+      
+      std::vector<size_t> elem_ind;
+      std::vector<size_t> res;
+      
+      mPatch2D.get_vertex_element_indices(3, elem_ind);
+      res[0] = 2; res[1] = 3;
+      CPPUNIT_ASSERT(res==elem_ind);
+      
+      mPatch2D.get_vertex_element_indices(2, elem_ind);
+      res[0] = 1; res[1] = 2; res[2] = 3;
+      CPPUNIT_ASSERT(res==elem_ind);
+   }
+
 };
 
 
