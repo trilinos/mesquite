3848ce33cafbac59b033676443d8b6a0b969f8b5
tleurent <tleurent>
2002-11-20 18:13:03 +0000

    That's it for PatchData for now.
    One test fails because the function PatchData::get_vertex_element_indices() is not implemented properly yet.

--- testSuite/unit/PatchDataTest.cpp
+++ testSuite/unit/PatchDataTest.cpp
@@ -8,7 +8,7 @@
 //    E-MAIL: tleurent@mcs.anl.gov
 //
 // ORIG-DATE: 13-Nov-02 at 18:05:56
-//  LAST-MOD: 20-Nov-02 at 10:52:57 by Thomas Leurent
+//  LAST-MOD: 20-Nov-02 at 12:11:19 by Thomas Leurent
 //
 // DESCRIPTION:
 // ============
@@ -38,7 +38,9 @@ private:
    CPPUNIT_TEST (test_reserve_vertex_capacity);
    CPPUNIT_TEST (test_reserve_element_capacity);
    CPPUNIT_TEST (test_get_element_vertex_indices);
-   CPPUNIT_TEST (test_get_vertex_element_indices);   
+   CPPUNIT_TEST (test_get_vertex_element_indices);
+   CPPUNIT_TEST (test_get_element_vertex_coordinates);
+   CPPUNIT_TEST (test_move_free_vertices);
    CPPUNIT_TEST_SUITE_END();
    
 private:
@@ -60,6 +62,13 @@ public:
   void setUp()
   {
      MsqError err;
+
+     /* our 2D set up: 2 triangles and one quad are available
+       1___3___5
+        |\1|   |
+        |0\| 2 |
+       0---2---4
+     */
      vtx_0_0.set(0,0,0);
      vtx_0_1.set(0,1,0);
      vtx_1_0.set(1,0,0);
@@ -67,13 +76,6 @@ public:
      vtx_2_0.set(2,0,0);
      vtx_2_1.set(2,1,0);
 
-     /* our 2D set up: 2 triangles and one quad are available
-        
-       1___3___5
-        |\2|   |
-        |1\| 3 |
-       0---2---4
-     */
      mPatch2D.reserve_vertex_capacity(6, err); MSQ_CHKERR(err);
      mPatch2D.add_vertex(NULL, NULL, 0,0,0, true, err); MSQ_CHKERR(err);
      mPatch2D.add_vertex(NULL, NULL, 0,1,0, true, err); MSQ_CHKERR(err);
@@ -149,39 +151,97 @@ public:
       CPPUNIT_ASSERT_MESSAGE("vertex already existed.", !err.errorOn);
    }
 
-   void test_get_element_vertex_indices() {
+   void test_get_element_vertex_indices()
+   {
 
       MsqError err;
       
       std::vector<size_t> vtx_ind;
       std::vector<size_t> res;
-      
+
+      // test we get the right vertices for element 1 (tri)
       mPatch2D.get_element_vertex_indices(1, vtx_ind, err); MSQ_CHKERR(err);
       res.push_back(1); res.push_back(2); res.push_back(3);
       CPPUNIT_ASSERT( vtx_ind==res );
 
+      // test we get the right vertices for element 2 (quad)
       vtx_ind.clear(); res.clear();
       mPatch2D.get_element_vertex_indices(2, vtx_ind, err); MSQ_CHKERR(err);
       res.push_back(3); res.push_back(2); res.push_back(4); res.push_back(5);
       CPPUNIT_ASSERT( vtx_ind==res );
    }
 
-   void test_get_vertex_element_indices() {
+   void test_get_vertex_element_indices()
+   {
       MsqError err;
       
       std::vector<size_t> elem_ind;
       std::vector<size_t> res;
       
+      // test we get the elements contiguous to vertex 3
       mPatch2D.get_vertex_element_indices(3, elem_ind);
-      res.push_back(2); res.push_back(3);
+      res.push_back(1); res.push_back(2);
       CPPUNIT_ASSERT(res==elem_ind);
       
+      // test we get the elements contiguous to vertex 2
       elem_ind.clear(); res.clear();
       mPatch2D.get_vertex_element_indices(2, elem_ind);
-      res.push_back(1); res.push_back(2); res.push_back(3);
+      res.push_back(0); res.push_back(1); res.push_back(2);
       CPPUNIT_ASSERT(res==elem_ind);
    }
 
+   void test_get_element_vertex_coordinates()
+   {
+      MsqError err;
+
+      std::vector< Vector3D > coords;
+      mPatch2D.get_element_vertex_coordinates(1, coords,err);
+      
+      CPPUNIT_ASSERT( coords[0]==vtx_0_1 );
+      CPPUNIT_ASSERT( coords[1]==vtx_1_0 );
+      CPPUNIT_ASSERT( coords[2]==vtx_1_1 );
+   }
+
+   /* This tests the move_free_vertices() function as well as the
+      PatchDataCoordsMemento functionality
+      */
+   void test_move_free_vertices()
+   {
+      MsqError err;
+
+      // gets a memento of the patch coordinates.
+      PatchDataCoordsMemento* coords_mem = mPatch2D.create_coords_memento(err);
+      MSQ_CHKERR(err);
+      
+      // states that the 2 first vertices are free and move them in direction dk
+      // by step size s;
+      Vector3D dk[2];
+      dk[0].set(-1,-2,0);
+      dk[1].set(-1, 2,0);
+      double s = 0.3;
+      mPatch2D.set_num_free_vertices(2);
+      mPatch2D.move_free_vertices(dk, 2, s, err); MSQ_CHKERR(err);
+
+      // gets the new coordinates and  checks the vertices were displaced as expected.
+      std::vector< Vector3D > coords;
+      mPatch2D.get_element_vertex_coordinates(0, coords,err);
+      Vector3D new_vtx_0_0 = vtx_0_0 + s*dk[0];
+      Vector3D new_vtx_0_1 = vtx_0_1 + s*dk[1];
+      CPPUNIT_ASSERT(coords[0] == new_vtx_0_0);
+      CPPUNIT_ASSERT(coords[2] == new_vtx_0_1);
+
+      // restore the PatchData to previous coords.
+      mPatch2D.set_to_coords_memento(coords_mem, err); MSQ_CHKERR(err);
+
+      // gets the new coordinates and  checks the vertices are back to original.
+      coords.clear();
+      mPatch2D.get_element_vertex_coordinates(0, coords,err);
+      CPPUNIT_ASSERT(coords[0] == vtx_0_0);
+      CPPUNIT_ASSERT(coords[2] == vtx_0_1);
+
+      delete coords_mem;
+   }
+   
 };
 
 
