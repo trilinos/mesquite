b97e59d798b5bc17b760eb041c0b3e6a7d4beb5f
djmelan <djmelan>
2002-12-05 23:13:42 +0000

    Added a couple of classes, Mesquite::StopWatch and Mesquite::StopWatchCollection.  Also added Mesquite:: GlobalStopWatches, a global variable of type StopWatchCollection.
    The StopWatch class is just what it sounds like.  It's
    got the following functions:
    
      start();
      stop();
      reset();
      total_time();
    
    The StopWatchCollection provides a way to refer to one
    or more StopWatches by name.  It has the same functions
    as StopWatch, but with a name argument, such as:
    
      start(const std::string &name);
    
    There are also functions to add and remove stop watches.
    
    You can also refer to a stopwatch by Key (a value returned
    when you add a stopwatch to the collection).  Using a key is
    faster than using a string.  The key is returned when you
    first add a stopwatch to the collection, and can also be
    obtained using "get_key(name)".
    
    GlobalStopWatches is a global variable, so it's accessible
    anywhere, as long as you #include MsqTimer.hpp.  Here's an
    example of how to use it:
    
      Mesquite::GlobalStopWatches.add("My Stop Watch");
      Mesquite::GlobalStopWatches.start("My Stop Watch");
      // (Do some stuff...)
      double how_long = Mesquite::GlobalStopWatches.total_time("My Stop Watch");
    
      Mesquite::StopWatchCollection::Key my_key = Mesquite::GlobalStopWatches.get_key("My Stop Watch");
      Mesquite::GlobalStopWatches.reset(my_key);
      Mesquite::GlobalStopWatches.start(my_key);
    
    Since GlobalStopWatches is a global variable, the example functions above can be called from several different functions or files.  As long as you know the name or the key, you can use the same stopwatch in multiple places in the code.  And the stopwatches in the collection live until they are explicitly removed from the collection.

--- src/Misc/MsqTimer.cpp
+++ src/Misc/MsqTimer.cpp
@@ -1,5 +1,8 @@
 #include "MsqTimer.hpp"
 
+// Create the global collection of stop watches
+Mesquite::StopWatchCollection GlobalStopWatches;
+
 // Unfortunately, many Unix platforms don't have good support
 // for the ANSI standard clock() function.  That means we have
 // to implement this class differently for WIN32 and Unix.  If
@@ -64,7 +67,155 @@ double Mesquite::Timer::since_last_check()
   return rv;
 }
 
-double Mesquite::Timer::since_birth()
+double Mesquite::Timer::since_birth() const
 {
   return now() - atBirth;
 }
+
+void Mesquite::StopWatch::start()
+{
+  if (!isRunning)
+  {
+    isRunning = true;
+    timeAtLastStart=now();
+  }
+}
+
+void Mesquite::StopWatch::stop()
+{
+  if (isRunning)
+  {
+    isRunning = false;
+    totalTime += now() - timeAtLastStart;
+  }
+}
+
+void Mesquite::StopWatch::reset()
+{
+  isRunning=false;
+  totalTime=0;
+}
+
+double Mesquite::StopWatch::total_time() const
+{
+  double rv = totalTime;
+  if (isRunning)
+    rv += now() - timeAtLastStart;
+  return rv;
+}
+
+Mesquite::StopWatchCollection::Key Mesquite::StopWatchCollection::add(
+  const std::string &name,
+  bool fail_if_exists)
+{
+    // Don't allow empty name
+  if (name == "")
+    return 0;
+  
+  Key key = get_key(name);
+  
+    // If the named stopwatch doesn't exist...
+  if (!key)
+  {
+      // See if there is an unused existing stopwatch
+    int i;
+    for (i = 0; i < mEntries.size(); i++)
+    {
+      if (mEntries[i].first == "")
+      {
+        mEntries[i].first = name;
+        mEntries[i].second.reset();
+        break;
+      }
+    }
+      // If not, create a new one
+    if (i == mEntries.size())
+    {
+      mEntries.push_back(std::pair<std::string, StopWatch>(name, StopWatch()));
+    }
+    key = i+1;
+  }
+    // If it already existed...
+  else if (fail_if_exists)
+    key = 0;
+  
+  return key;
+}
+
+
+Mesquite::StopWatchCollection::Key Mesquite::StopWatchCollection::get_key(
+  const std::string &name) const
+{
+  Key key = 0;
+  
+  for (int i = 0; i < mEntries.size(); i++)
+  {
+    if (mEntries[i].first == name)
+    {
+      key = i + 1;
+      break;
+    }
+  }
+
+  return key;
+}
+
+void Mesquite::StopWatchCollection::remove(
+  const Mesquite::StopWatchCollection::Key key)
+{
+    // Get rid of anything at the end of the list
+  if (key == mEntries.size())
+  {
+    mEntries.pop_back();
+    while (!mEntries.empty() && mEntries.back().first == "")
+    {
+      mEntries.pop_back();
+    }
+  }
+  
+  else if (key > 0 && key < mEntries.size())
+  {
+      // If in the middle of the list, set its name to ""
+    mEntries[key-1].first = "";
+  }
+}
+
+
+void Mesquite::StopWatchCollection::start(
+  const Mesquite::StopWatchCollection::Key key)
+{
+  if (key > 0 &&
+      key <= mEntries.size() &&
+      mEntries[key-1].first != "")
+    mEntries[key-1].second.start();
+}
+
+void Mesquite::StopWatchCollection::stop(
+  const Mesquite::StopWatchCollection::Key key)
+{
+  if (key > 0 &&
+      key <= mEntries.size() &&
+      mEntries[key-1].first != "")
+    mEntries[key-1].second.stop();
+}
+
+void Mesquite::StopWatchCollection::reset(
+  const Mesquite::StopWatchCollection::Key key)
+{
+  if (key > 0 &&
+      key <= mEntries.size())
+    mEntries[key-1].second.reset();
+}
+
+
+double Mesquite::StopWatchCollection::total_time(
+  const Mesquite::StopWatchCollection::Key key) const
+{
+  if (key > 0 &&
+      key <= mEntries.size() &&
+      mEntries[key-1].first != "")
+    return mEntries[key-1].second.total_time();
+  else
+    return 0.0;
+}
+
--- src/Misc/MsqTimer.hpp
+++ src/Misc/MsqTimer.hpp
@@ -1,6 +1,10 @@
 #ifndef MESQUITE_TIMER_HPP
 #define MESQUITE_TIMER_HPP
 
+#include <vector>
+#include <utility>
+#include <string>
+
 namespace Mesquite
 {
   class Timer
@@ -8,18 +12,105 @@ namespace Mesquite
   public:
     Timer();
     
-    double since_last_check(); //- return CPU time in seconds since last
+    double since_last_check(); //- return time in seconds since last
                                //- call to since_last_check().  Note that
                                //- calling since_birth() doesn't count
-                               //- as a check.
+                               //- as a check.  The first time this function
+                               //- is called, it returns the time since birth.
     
-    double since_birth();      //- return CPU time in seconds since
+    double since_birth() const;//- return time in seconds since
                                //- object was created.
     
   private:
     const double atBirth;      //- Time at birth
     double atLastCheck;        //- Time at last call to since_last_check()
   };
+  
+  
+  class StopWatch
+  {
+  public:
+      // Creates the stopwatch.  The stopwatch is stopped
+      // until start() is called.
+    StopWatch() 
+        :isRunning(false), totalTime(0)
+      {}
+    
+      // Starts the stopwatch.  If it was already running,
+      // this function does nothing.
+    void start();
+    
+      // Stops the stopwatch.  If it was not already running,
+      // this function does nothing
+    void stop();
+    
+      // Stops the stopwatch and resets the total_time() to zero.
+    void reset();
+    
+      // Returns the total accumulated time.  If the stopwatch
+      // is currently running, the time between the last start()
+      // and the current time IS included in total_time().
+    double total_time() const;
+    
+  private:
+    bool isRunning;
+    double timeAtLastStart;
+    double totalTime;
+  };
+  
+  class StopWatchCollection
+  {
+  public:
+    typedef size_t Key;
+    
+      // Create a new collection
+    StopWatchCollection()
+      {}
+    
+      // Add a stopwatch to the collection.  Returns a non-zero
+      // StopWatchCollection::Key if it succeeds, zero if it fails.
+      // If a StopWatch with the given name already exists in the
+      // collection, the Key of the existing StopWatch is returned
+      // if 'fail_if_exists' is false, or zero is returned if
+      // 'fail_if_exists' is true.
+    Key add(const std::string &name, bool fail_if_exists = true);
+    
+      // Gets the Key for an existing stopwatch.  If a stopwatch
+      // with the given name does not exist, function returns zero.
+    Key get_key(const std::string &name) const;
+    
+      // Remove a specific stopwatch.
+    void remove(const Key key);
+    void remove(const std::string &name)
+      { remove(get_key(name)); }
+    
+      // start a specific stopwatch
+    void start(const Key key);
+    void start(const std::string &name)
+      { start(get_key(name)); }
+    
+      // stop a specific stopwatch
+    void stop(const Key key);
+    void stop(const std::string &name)
+      { stop(get_key(name)); }
+    
+      // reset a specific stopwatch
+    void reset(const Key key);
+    void reset(const std::string &name)
+      { reset(get_key(name)); }
+    
+      // Get the total time for a specific stopwatch, zero if
+      // the stopwatch doesn't exist.
+    double total_time(const Key key) const;
+    double total_time(const std::string &name) const
+      { return total_time(get_key(name)); }
+    
+  private:
+    std::vector< std::pair<std::string, StopWatch> > mEntries;
+  };
+  
+    // A stopWatchCollection available anywhere
+  extern Mesquite::StopWatchCollection GlobalStopWatches;
 }
 
 #endif
