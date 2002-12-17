b2c3776c17b6096305f96594bea4236cca4ce62a
tleurent <tleurent>
2002-12-17 22:55:46 +0000

    adding a driver that test the LPTemplate analytical gradient on a 3D mesh (hexes) with 3 free vertices.
    The code works except when the nodes start in a perfect position.
    We must be mishandling a 0 returned by the analytical quality metric.
    Mike will look at it tonight.

---
+++ testSuite/analytical_grad_3D/main.cpp
@@ -0,0 +1,117 @@
+// -*- Mode : c++; tab-width: 3; c-tab-always-indent: t; indent-tabs-mode: nil; c-basic-offset: 3 -*-
+//
+//   SUMMARY: 
+//     USAGE:
+//
+// ORIG-DATE: 19-Feb-02 at 10:57:52
+//  LAST-MOD:  3-Dec-02 at 11:29:33 by Thomas Leurent
+//
+//
+// DESCRIPTION:
+// ============
+/*! \file main.cpp
+
+describe main.cpp here
+
+ */
+// DESCRIP-END.
+//
+
+#ifdef USE_STD_INCLUDES
+#include <iostream>
+#else
+#include <iostream.h>
+#endif
+
+#ifdef USE_C_PREFIX_INCLUDES
+#include <cstdlib>
+#else
+#include <stdlib.h>
+#endif
+
+
+#include "Mesquite.hpp"
+#include "TSTT_Base.h"
+#include "MesquiteUtilities.hpp" //  for writeShowMeMesh()
+#include "MesquiteError.hpp"
+#include "Vector3D.hpp"
+#include "InstructionQueue.hpp"
+#include "MeshSet.hpp"
+#include "PatchData.hpp"
+#include "StoppingCriterion.hpp"
+#include "QualityAssessor.hpp"
+
+// algorythms
+#include "MeanRatioQualityMetric.hpp"
+#include "LPTemplate.hpp"
+#include "LInfTemplate.hpp"
+#include "SteepestDescent.hpp"
+
+using namespace Mesquite;
+
+
+#undef __FUNC__
+#define __FUNC__ "main"
+int main()
+{     
+  char file_name[128];
+  /* Reads a TSTT Mesh file */
+  TSTT::Mesh_Handle mesh;
+  TSTT::MeshError tstt_err;
+  TSTT::Mesh_Create(&mesh, &tstt_err);
+  strcpy(file_name, "../../meshFiles/3D/VTK/hexes_4by2by2.vtk");
+  TSTT::Mesh_Load(mesh, file_name, &tstt_err);
+  
+  // Mesquite error object
+  MsqError err;
+  
+  // initialises a MeshSet object
+  MeshSet mesh_set1;
+  mesh_set1.add_mesh(mesh, err); MSQ_CHKERR(err);
+
+  // dbg
+   std::cout << " TSTT mesh handle: " << mesh << std::endl;
+  
+  // creates an intruction queue
+  InstructionQueue queue1;
+
+  // creates a mean ratio quality metric ...
+  ShapeQualityMetric* mean_ratio = MeanRatioQualityMetric::create_new();
+  mean_ratio->set_averaging_method(QualityMetric::LINEAR, err);
+  mean_ratio->set_gradient_type(QualityMetric::ANALYTICAL_GRADIENT);
+//  mean_ratio->set_gradient_type(QualityMetric::NUMERICAL_GRADIENT);
+   
+  // ... and builds an objective function with it
+    //LInfTemplate* obj_func = new LInfTemplate(mean_ratio);
+  LPTemplate* obj_func = new LPTemplate(mean_ratio, 2, err);
+//   obj_func->set_gradient_type(ObjectiveFunction::NUMERICAL_GRADIENT);
+   obj_func->set_gradient_type(ObjectiveFunction::ANALYTICAL_GRADIENT);
+   // creates the steepest descent optimization procedures
+  SteepestDescent* pass1 = new SteepestDescent( obj_func );
+  pass1->set_patch_type(PatchData::GLOBAL_PATCH, err);
+  pass1->set_maximum_iteration(6);
+
+  QualityAssessor stop_qa=QualityAssessor(mean_ratio,QualityAssessor::MAXIMUM);
+
+   //**************Set stopping criterion****************
+// StoppingCriterion sc1(&stop_qa,1.0,1.8);
+ StoppingCriterion sc2(StoppingCriterion::NUMBER_OF_PASSES,1);
+// CompositeAndStoppingCriterion sc(&sc1,&sc2);
+ pass1->set_stopping_criterion(&sc2);
+ // sets a culling method on the first QualityImprover
+ pass1->add_culling_method(PatchData::NO_BOUNDARY_VTX);
+
+  // adds 1 pass of pass1 to mesh_set1
+//  queue1.add_preconditioner(pass1, err); MSQ_CHKERR(err);
+  queue1.set_master_quality_improver(pass1, err); MSQ_CHKERR(err);
+  // adds 1 passes of pass2 to mesh_set1
+//  mesh_set1.add_quality_pass(pass2);
+
+  writeVtkMesh("original_mesh", mesh, err); MSQ_CHKERR(err);
+  
+  // launches optimization on mesh_set1
+  queue1.run_instructions(mesh_set1, err); MSQ_CHKERR(err);
+  
+  writeVtkMesh("smoothed_mesh", mesh, err); MSQ_CHKERR(err);
+
+}
