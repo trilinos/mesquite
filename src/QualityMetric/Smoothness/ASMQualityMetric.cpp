0f903e00f32453d3b5f4474b1d7b5202bb4f4d6d
brewer <brewer>
2003-01-14 22:54:59 +0000

    Adding first draft of the Area Smoothness Metric (hop metric).  This metric has not been tested thoroughly, yet.

---
+++ src/QualityMetric/Smoothness/ASMQualityMetric.cpp
@@ -0,0 +1,95 @@
+/*!
+  \file   ASMQualityMetric.cpp
+  \brief  QualityMetric class for ASM (Area Smoothness Metric)
+
+  \author Michael Brewer
+  \date   2002-06-9
+*/
+#include <vector>
+#include "ASMQualityMetric.hpp"
+#include <math.h>
+#include "Vector3D.hpp"
+#include "ShapeQualityMetric.hpp"
+#include "QualityMetric.hpp"
+#include "MsqMessage.hpp"
+using namespace Mesquite;
+
+#undef __FUNC__
+#define __FUNC__ "ASMQualityMetric::ASMQualityMetric"
+
+ASMQualityMetric::ASMQualityMetric()
+{
+  MsqError err;
+  set_metric_type(ELEMENT_BASED);
+  set_element_evaluation_mode(ELEMENT_VERTICES, err);
+  MSQ_CHKERR(err);
+  avgMethod=QualityMetric::MAXIMUM;
+  feasible=0;
+  set_name("Area Smoothness");
+}
+
+bool ASMQualityMetric::evaluate_element(PatchData &pd,
+                                                    MsqMeshEntity *element,
+                                                    double &fval,
+                                                    MsqError &err)
+{
+  double temp_double;
+  size_t elem_ind=pd.get_element_index(element);
+  std::vector<size_t> adj_elems;
+ 
+  MsqMeshEntity *elems = pd.get_element_array(err);
+  MsqVertex *vertices=pd.get_vertex_array(err);
+  switch(element->get_element_type()){
+    case TRIANGLE:
+    case QUADRILATERAL:
+      pd.get_adjacent_entities_via_n_dim(1,elem_ind,adj_elems,err);
+      break;
+    case TETRAHEDRON:
+    case HEXAHEDRON:
+      pd.get_adjacent_entities_via_n_dim(2,elem_ind,adj_elems,err);
+      break;
+    default:
+      err.set_msg("ASM quality metric not implemented for this element type.");
+  };
+  int num_samp=adj_elems.size();
+  double* met_vals = new double [num_samp];
+  int i=0;
+  switch(element->get_element_type()){
+    case TRIANGLE:
+    case QUADRILATERAL:
+      temp_double=element->compute_unsigned_area(pd,err);
+        //PRINT_INFO("\nunsigned area = %f",temp_double);
+      for(i=0;i<num_samp;++i){
+        met_vals[i]=elems[adj_elems[i]].compute_unsigned_area(pd,err);
+          //PRINT_INFO("  neighboring nunsigned area = %f",met_vals[i]);
+        if((temp_double+met_vals[i])>MSQ_MIN){
+          met_vals[i]=fabs((temp_double-met_vals[i])/(temp_double+met_vals[i]));
+        }
+        else
+          met_vals[i]=0.0;
+      }
+      break;                                
+                                               
+    case TETRAHEDRON:
+    case HEXAHEDRON:
+      temp_double=element->compute_unsigned_volume(pd,err);
+      for(i=0;i<num_samp;++i){
+        met_vals[i]=elems[adj_elems[i]].compute_unsigned_volume(pd,err);
+        if((temp_double+met_vals[i])>MSQ_MIN){
+          met_vals[i]=fabs((temp_double-met_vals[i])/(temp_double+met_vals[i]));
+        }
+        else
+          met_vals[i]=0.0;
+      }
+      break;
+    default:
+      err.set_msg("ASM quality metric not implemented for this element type.");
+  };
+  fval=average_metrics(met_vals,num_samp,err);
+    //PRINT_INFO("\nRETURNING %f \n",fval);
+  delete []met_vals;
+      
+  return true;
+}
+
+
---
+++ src/QualityMetric/Smoothness/ASMQualityMetric.hpp
@@ -0,0 +1,73 @@
+// -*- Mode : c++; tab-width: 3; c-tab-always-indent: t; indent-tabs-mode: nil; c-basic-offset: 3 -*-
+
+/*! \file ASMQualityMetric.hpp
+
+Header file for the Mesquite::ASMQualityMetric class
+
+  \author Michael Brewer
+  \date   2002-06-19
+ */
+
+
+#ifndef ASMQualityMetric_hpp
+#define ASMQualityMetric_hpp
+#include "MsqMeshEntity.hpp"
+#include "Mesquite.hpp"
+#include "MesquiteError.hpp"
+#include "SmoothnessQualityMetric.hpp"
+#include "Vector3D.hpp"
+#include "PatchData.hpp"
+
+
+namespace Mesquite
+{
+     /*! \class ASMQualityMetric
+       \brief Computes the ASM (Area Smoothness Metric) of the given element.
+        The metric does not use the sample point functionality or the
+        compute_weighted_jacobian.  It computes the unsigned area
+        or volume (a_0)of the element and of neighboring elements (a_i).
+        In 2-d, elements are considered neighbors if they share an
+        edge.  In 3-d, elements are considered neighbors if they
+        share a face.  The metric is then taken to be the average
+        (default is MAXIMUM) of abs(a_i-a_0)/(a_i+a_0) , and the metric
+        needs to be minimized.  The ideal metic value is zero which occurs
+        when an element and its neighbors have the same unsigned area
+        (or volume in three-dimensions).
+     */
+   class ASMQualityMetric : public SmoothnessQualityMetric
+   {
+  public:
+ 
+       /*!Returns a pointer to a SmoothnessQualityMetric. 
+         
+       */
+     static SmoothnessQualityMetric* create_new(){
+       
+       SmoothnessQualityMetric* m = new ASMQualityMetric();
+       return m;
+     }
+     
+       //! virtual destructor ensures use of polymorphism during destruction
+     virtual ~ASMQualityMetric()
+        {}
+     
+       //! evaluate using mesquite objects 
+     bool evaluate_element(PatchData &pd, MsqMeshEntity *element,double &fval,
+                           MsqError &err); 
+          
+  protected:
+  private:
+     
+     ASMQualityMetric();
+    
+  };
+
+   
+   
+
+} //namespace
+
+
+#endif // ASMQualityMetric_hpp
+
+
