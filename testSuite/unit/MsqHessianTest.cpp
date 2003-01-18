c4a5cf8498701d450b1660c9a9a14581db1f4d61
tleurent <tleurent>
2003-01-18 00:10:09 +0000

    since the Hessian is symetric, MsqHessian now only keeps the upper triangular blocks.

--- testSuite/unit/MsqHessianTest.cpp
+++ testSuite/unit/MsqHessianTest.cpp
@@ -8,7 +8,7 @@
 //    E-MAIL: tleurent@mcs.anl.gov
 //
 // ORIG-DATE: 13-Jan-03 at 09:05:56
-//  LAST-MOD: 17-Jan-03 at 11:46:27 by Thomas Leurent
+//  LAST-MOD: 17-Jan-03 at 18:08:40 by Thomas Leurent
 //
 // DESCRIPTION:
 // ============
@@ -70,19 +70,19 @@ public:
     MsqHessian::initialize(twoTriangles, err); MSQ_CHKERR(err);
 
     // Checks values of mRowStart are correct.
-    int row_start[] = {0, 4, 8, 11, 14};
+    int row_start[] = {0, 4, 7, 8, 9};
     for (int i=0; i<5; ++i) 
-      CPPUNIT_ASSERT( mRowStart[i]  == row_start[i] );
+      CPPUNIT_ASSERT( mRowStart[i] == row_start[i] );
 
     // Checks values of mColIndex are correct.
-    int col_index[] = {0,1,2,3,0,1,2,3,0,1,2,0,1,3};
-    for (int i=0; i<14; ++i) 
-      CPPUNIT_ASSERT( mColIndex[i]  == col_index[i] );
+    int col_index[] = {0,1,2,3,1,2,3,2,3};
+    for (int i=0; i<9; ++i) 
+      CPPUNIT_ASSERT( mColIndex[i] == col_index[i] );
 
-    // Checks values of mColInstr are correct. 
-    int col_instr[] = {0,1,2,4,5,6,8,9,10,0,3,1,11,13,12,4,7,5};
-    for (int i=0; i<18; ++i)
-      CPPUNIT_ASSERT( mColInstr[i]  == col_instr[i] );
+    // Checks values of mAccumulation are correct. 
+    int col_instr[] = {0,1,2,4,5,7,0,3,1,8,-6,4};
+    for (int i=0; i<12; ++i)
+      CPPUNIT_ASSERT( mAccumulation[i] == col_instr[i] );
 
   }
 
