70e0e9dd92f409e36340befaba7df3cb0d9017c8
tleurent <tleurent>
2003-02-18 23:10:13 +0000

    The conjugate gradient algebraic solver is now in the Feasible Newton algorithm. It compiles but has not been tested.
    We might want to integrate the solver within the Hessian object in order to avoid reallocating temporary arrays every time a Patch is needed.

--- src/QualityImprover/VertexMover/FeasibleNewton/FeasibleNewton.cpp
+++ src/QualityImprover/VertexMover/FeasibleNewton/FeasibleNewton.cpp
@@ -5,7 +5,7 @@
 //    E-MAIL: tleurent@mcs.anl.gov
 //
 // ORIG-DATE: 15-Jan-03 at 08:05:56
-//  LAST-MOD: 31-Jan-03 at 16:57:51 by Thomas Leurent
+//  LAST-MOD: 18-Feb-03 at 17:05:40 by Thomas Leurent
 //
 // DESCRIPTION:
 // ============
@@ -36,6 +36,7 @@ FeasibleNewton::FeasibleNewton(ObjectiveFunction* of) :
   MsqError err;
   gradientLessThan=.001;
   maxIteration=6;
+  maxCGiter = 1000;
   this->set_name("FeasibleNewton");
   set_patch_type(PatchData::GLOBAL_PATCH, err);
 }  
@@ -64,14 +65,22 @@ void FeasibleNewton::optimize_vertex_positions(PatchData &pd,
 {
   PRINT_INFO("\no  Performing Feasible Newton optimization.\n");
   int num_free_vertices = pd.num_free_vertices(err);
-  int num_vertices = pd.num_vertices();
-  Vector3D* grad = new Vector3D[num_vertices];
+  int nv = pd.num_vertices();
+  Vector3D* grad = new Vector3D[nv];
   bool fn_bool=true;// bool used for determining validity of patch
   /* Computes the value of the stopping criterion*/
   MeshSet *mesh=get_mesh_set();
   bool inner_criterion=inner_criterion_met(*mesh,err);
-  int n;
+  int n, i;
   
+  // variables used in conjugate gradient solver
+  Vector3D* d = new Vector3D[nv];
+  Vector3D* r = new Vector3D[nv];
+  Vector3D* z = new Vector3D[nv];
+  Vector3D* p = new Vector3D[nv];
+  Vector3D* w = new Vector3D[nv];
+  double alpha_, alpha, beta; 
+
   // 1.  Allocate a hessian and calculate the sparsity pattern.
   mHessian.initialize(pd, err); MSQ_CHKERR(err);
   // 2.  Calculate the gradient and Hessian for the patch
@@ -82,7 +91,7 @@ void FeasibleNewton::optimize_vertex_positions(PatchData &pd,
   if (!fn_bool) { err.set_msg("invalid patch for gradient calculation"); return; }
   // 3.  Calculate the norm of the gradient for the patch
   double grad_norm=0;
-  for (n=0; n<num_vertices; ++n) 
+  for (n=0; n<nv; ++n) 
     grad_norm += grad[n] % grad[n]; // dot product
   grad_norm = sqrt(grad_norm);
   MSQ_DEBUG_ACTION(3,{std::cout<< "  o  gradient norm: " << grad_norm << std::endl;});
@@ -135,6 +144,48 @@ void FeasibleNewton::optimize_vertex_positions(PatchData &pd,
     //    (a) stop if conjugate iteration limit reached
     //    (b) stop if relative residual is small
     //    (c) stop if direction of negative curvature is obtained
+    double cg_tol =10e-2;
+    double norm_g = length(grad, nv);
+    double norm_r = norm_g;
+    double rzm1; // r^T_{k-1} z_{k-1}
+    double rzm2; // r^T_{k-2} z_{k-2}
+    mHessian.compute_preconditionner(err); MSQ_CHKERR(err); // get M^{-1}
+
+    for (i=0; i<nv; ++i)  d[i] = 0. ;  
+    for (i=0; i<nv; ++i)  r[i] = -grad[i] ;  // r = -g because x_0 = 0 (and b=-g) 
+    norm_g *= cg_tol;
+
+    mHessian.apply_preconditionner(z, r, err); // solve Mz = r (computes z = M^-1 r)
+    for (i=0; i<nv; ++i)  p[i] = z[i] ; // p_1 = z_0  
+    rzm1 = inner(z,r,nv); // inner product r_{k-1}^T z_{k-1} 
+    
+    int cg_iter = 0;
+    while ((norm_r > norm_g) && (cg_iter < maxCGiter)) {
+      ++cg_iter;
+      
+      axpy(w, nv, mHessian, p, nv, 0,0,err); // w = A * p_k
+      
+      alpha_ = inner(p,w,nv); // alpha_ = p_k^T A p_k
+      if (alpha_ <= 0.0) {
+        printf("Direction of Negative Curvature\n");
+        break; // Newton goes on with this direction of negative curvature 
+      }
+      
+      alpha = rzm1 / alpha_;
+      
+      for (i=0; i<nv; ++i)  d[i] += alpha*p[i]; // x_{k+1} = d_k + alpha_{k+1} p_{k+1} 
+      for (i=0; i<nv; ++i)  r[i] -= alpha*w[i]; // r_{k+1} = r_k - alpha_{k+1} A p_{k+1} 
+      norm_r = length(r, nv);
+      
+      mHessian.apply_preconditionner(z, r, err); // solve Mz = r (computes z = M^-1 r)
+      
+      rzm2 = rzm1;
+      rzm1 = inner(z,r,nv); // inner product r_{k-1}^T z_{k-1} 
+      beta = rzm1 / rzm2;
+      for (i=0; i<nv; ++i)  p[i] = z[i] + beta*p[i]; // p_k = z_{k-1} + Beta_k * p_{k-1}
+    }
+
+
     // 6. Check for descent direction (inner produce of gradient and
     //    direction is negative.
     // 7. Search along the direction
