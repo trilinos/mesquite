d79c2376959bbd70d29ca85d2458e277f067a71b
brewer <brewer>
2003-07-08 19:13:31 +0000

    Replacing the old copyright information with the new version.  This is in Mesquite.hpp and mainpage.h (the mainpage of the generated doxygen doc.)
    We still need to add the LGPL license agreement language somewhere.

--- include/Mesquite.hpp
+++ include/Mesquite.hpp
@@ -31,12 +31,11 @@
 
 /*!
   \namespace Mesquite
-  
-         Copyright 2002 Sandia Corporation. Under the terms of 
-         Contract DE-AC04-94AL85000, there is a non-exclusive
-         license for use of this work by or on behalf of the U.S.
-         Government. Export of this program may require a license
-         from the United States Government.
+  Copyright 2003 Sandia Corporation and the University of Chicago. Under
+  the terms of Contract DE-AC04-94AL85000 with Sandia Corporation and
+  Contract W-31-109-ENG-38 with the University of Chicago, the U.S.
+  Government retains certain rights in this software.
+
 */
 namespace Mesquite
 {
