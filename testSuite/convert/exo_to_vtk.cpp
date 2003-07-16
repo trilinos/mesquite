a92927b857a7f29809d95596bf0c7d2babf58997
brewer <brewer>
2003-07-16 17:19:49 +0000

    Adding exo_to_vtk and vtk_to_exo to convert between the two file formats.
    Not adding this directory to the main makefile since this
    functionality requires the exodus libs.  If they are compiled
    without those libraries, an error will be printed upon
    execution.

---
+++ testSuite/convert/exo_to_vtk.cpp
@@ -0,0 +1,73 @@
+// -*- Mode : c++; tab-width: 3; c-tab-always-indent: t; indent-tabs-mode: nil; c-basic-offset: 3 -*-
+//
+//   SUMMARY: 
+//     USAGE:
+//
+// ORIG-DATE: 19-Feb-02 at 10:57:52
+//  LAST-MOD: 23-May-03 at 18:04:38 by Thomas Leurent
+//
+//
+// DESCRIPTION:
+// ============
+/*! \file main.cpp
+
+describe main.cpp here
+
+ */
+// DESCRIP-END.
+//
+
+#ifdef USE_STD_INCLUDES
+#include <iostream>
+#else
+#include <iostream.h>
+#endif
+
+#ifdef USE_C_PREFIX_INCLUDES
+#include <cstdlib>
+#else
+#include <stdlib.h>
+#endif
+
+
+#include "Mesquite.hpp"
+#include "MeshImpl.hpp"
+#include "MesquiteError.hpp"
+
+using namespace Mesquite;
+
+using std::cout;
+using std::endl;
+
+#undef __FUNC__
+#define __FUNC__ "main"
+int main(int argc, char* argv[])
+{
+  Mesquite::MsqError err;
+  char in_file_name[256];
+  char out_file_name[256];
+  double OF_value = 1.;
+  
+  // command line arguments
+  if (argc!=3){
+    cout << "Input meshfile name needed as first argument.\n"
+      "Output meshfile name needed as second argument.\n" << endl;
+    return -1;
+  }
+  else{
+    cout << " given 2 command line arguments.\n";
+    strcpy(in_file_name, argv[1]);
+    strcpy(out_file_name, argv[2]);
+  }
+#ifndef MSQ_USING_EXODUS
+  err.set_msg("Exodus not enabled in this build of Mesquite");
+  MSQ_CHKERR(err);
+  return -1;
+#else
+  Mesquite::MeshImpl *mesh = new Mesquite::MeshImpl;
+  cout<<"\nReading Exodus file.\n";
+  mesh->read_exodus(in_file_name, err);MSQ_CHKERR(err);
+  cout<<"Writing VTK file.\n";
+  mesh->write_vtk(out_file_name,err);MSQ_CHKERR(err);
+#endif
+}
