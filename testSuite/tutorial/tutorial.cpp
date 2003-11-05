cf823d6fd7f2ccfc27e03a7be60efa6895fc67d2
tleurent <tleurent>
2003-11-05 23:30:14 +0000

    the template file now takes a mesh file name as argument

--- testSuite/tutorial/tutorial.cpp
+++ testSuite/tutorial/tutorial.cpp
@@ -3,6 +3,17 @@ using namespace Mesquite;
 int main(int argc, char* argv[])
 {
   MsqError err;
+
+  char mesh_file_name[256];
   
-  
+  // command line arguments
+  if (argc==1 || argc>2) {
+    std::cerr << "meshfile name needed as argument.\n";
+    exit(EXIT_FAILURE);
+  }
+  else if (argc==2) {
+    std::cout << "Working with mesh file: "<< mesh_file_name << "\n";
+    strcpy(mesh_file_name, argv[1]);
+  } 
+
 }
