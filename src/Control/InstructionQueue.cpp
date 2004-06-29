05d43ce254848e8ad2ab9aca32f24aa9218c4735
tleurent <tleurent>
2004-06-29 17:13:12 +0000

    printing out version number when running.

--- src/Control/InstructionQueue.cpp
+++ src/Control/InstructionQueue.cpp
@@ -251,6 +251,8 @@ void InstructionQueue::set_master_quality_improver(QualityImprover* instr,
 #define __FUNC__ "InstructionQueue::run_instructions"
 void InstructionQueue::run_instructions(MeshSet &ms, MsqError &err)
 { 
+  PRINT_INFO(version_string(false));
+
   if (nbPreConditionners != 0 && isMasterSet == false ) {
     err.set_msg("no pre-conditionners allowed if master QualityImprover is not set.");
     return;
