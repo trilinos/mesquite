4063a48b164a1731c86fcd850a2d6d8d76bfc227
Jason A. Kraftcheck <kraftche@cae.wisc.edu>
2007-06-13 17:15:00 +0000

    Add Mesquite::Mesh implementation that is an adaptor for the new ITAPS mesh interface (iMesh).
     Makefile.am                  |    9
     configure.in                 |   32
     itaps/iBase.h                |  137 ++++
     itaps/iMesh.h                |  622 +++++++++++++++++++
     itaps/iMesh_FCDefs.h.in      |    7
     itaps/iMesh_protos.h         |  108 +++
     src/Mesh/Makefile.am         |    2
     src/Mesh/MsqIMesh.cpp        | 1407 +++++++++++++++++++++++++++++++++++++++++++
     src/Mesh/MsqIMesh.hpp        |   75 ++
     testSuite/unit/Makefile.am   |   12
     testSuite/unit/iMeshTest.cpp |  624 +++++++++++++++++++
     11 files changed, 3031 insertions(+), 4 deletions(-)

---
+++ src/Mesh/MsqIMesh.hpp
@@ -0,0 +1,75 @@
+/* ***************************************************************** 
+    MESQUITE -- The Mesh Quality Improvement Toolkit
+
+    Copyright 2007 Sandia National Laboratories.  Developed at the
+    University of Wisconsin--Madison under SNL contract number
+    624796.  The U.S. Government and the University of Wisconsin
+    retain certain rights to this software.
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License 
+    (lgpl.txt) along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    (2007) kraftche@cae.wisc.edu    
+
+  ***************************************************************** */
+
+
+/** \file MsqIMesh.hpp
+ *  \brief Adaptor for ITAPS iMesh interface
+ *  \author Jason Kraftcheck 
+ */
+
+#ifndef MSQ_I_MESH_HPP
+#define MSQ_I_MESH_HPP
+
+#include "MeshInterface.hpp"
+#include "iMesh.h"
+#include "MeshTSTT.hpp" // for tag name constants
+
+namespace Mesquite {
+
+/**\class MsqIMesh
+ *\brief Mesquite iMesh Adapter
+ *
+ * Adpater for interfacing Mesquite with an application that provides
+ * the ITAPS iMesh interface for interacting with mesh data.
+ */
+class MsqIMesh : public Mesquite::Mesh
+{
+public:
+
+  /**\brief factory method
+   *
+   * Create an MsqIMesh instance.
+   */
+  static MsqIMesh* create( iMesh_Instance imesh, 
+                           iBase_EntitySetHandle meshset,
+                           MsqError& err );
+  
+  /**\brief factory method
+   *
+   * Create an MsqIMesh instance with no initial mesh.
+   * Call set_active_set to complete initialization.
+   */
+  static MsqIMesh* create( iMesh_Instance imesh, MsqError& err );
+  
+  virtual void set_active_set( iBase_EntitySetHandle, MsqError& err ) = 0;
+  
+  virtual ~MsqIMesh() = 0;
+};  
+
+
+} // namespace Mesquite
+
+#endif
