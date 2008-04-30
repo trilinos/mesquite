2c0abce309261532a9f9040bb779d3a4e2c11ffc
Jason A. Kraftcheck <kraftche@cae.wisc.edu>
2008-04-30 15:55:29 +0000

    First pass of TrustRegion cleanup:   - remove #ifdef'd out and commented out code from FeasNewt that     is not used in Mesquite   - reorder logic to remove extra evaluations of objective function

--- src/QualityImprover/VertexMover/TrustRegion/TrustRegion.cpp
+++ src/QualityImprover/VertexMover/TrustRegion/TrustRegion.cpp
@@ -147,15 +147,11 @@ void TrustRegion::optimize_vertex_positions( PatchData& pd, MsqError& err )
   
   double radius = 1000;		/* delta*delta */
 
-  //Precond  *prec;
-
-  //double *v;
   Vector3D *w;
   Vector3D *z;
   Vector3D *d;
   Vector3D *p;
   Vector3D *r;
-  //Vector3D *t;
 
   double norm_r, norm_g;
   double alpha, beta, kappa;
@@ -164,174 +160,28 @@ void TrustRegion::optimize_vertex_positions( PatchData& pd, MsqError& err )
   double obj, objn;
 
   const int nn = pd.num_free_vertices(); //const int nn = mesh->nn;
-  int iter = 0, cg_iter;
-  int nf = 1, ng = 1, nh = 0;
-  int needH = 1;
-  int ferr;
-
-  //double m, m1, m2;
-  //double s, s1, s2;
-  //double t1, t2;
-
-  //struct rusage r0, r1;
-
-#ifdef DISPLAY_MAX
-  double fmax;
-  int    fidx;
-#endif
-
-  if (nn <= 0) {
-    /* No nodes!  Just return.                                               */
-    return; //return 0;
-  }
-
-  //getrusage(RUSAGE_SELF, &r0);
-
-#ifdef USE_WEIGHT
-  mesh->w = (double *)malloc(16*sizeof(double)*mesh->ne);
-
-  {
-    double *weight = mesh->w;
-
-    double W0[3];
-    double W1[3];
-    double W2[3];
-    double W3[3];
-    double Wmat[3][3];
-    double Q[3][3];
-    double R[3][3];
-    double Rinv[3][3];
-
-    int i;
-
-    srand48(1001);
-
-    for (i = 0; i < mesh->ne; ++i) {
-
-#ifdef RANDOM_WEIGHT
-      W0[0] = 0.0;
-      W0[1] = 0.0;
-      W0[2] = 0.0;
-
-      W1[0] = 5.0*drand48() + 0.5;
-      W1[1] = 0.0;
-      W1[2] = 0.0;
-
-      W2[0] = 10.0*drand48() - 5.0;
-      W2[1] = 5.0*drand48() + 0.5;
-      W2[2] = 0.0;
-
-      W3[0] = 10.0*drand48() - 5.0;
-      W3[1] = 10.0*drand48() - 5.0;
-      W3[2] = 5.0*drand48() + 0.5;
-#else
-      W0[0] = 0.0;
-      W0[1] = 0.0;
-      W0[2] = 0.0;
-
-      W1[0] = 1.0;
-      W1[1] = 0.0;
-      W1[2] = 0.0;
-
-      W2[0] = 0.5;
-      W2[1] = 0.5 * sqrt(3.0);
-      W2[2] = 0.0;
-
-      W3[0] = 0.5;
-      W3[1] = 0.5 / sqrt(3.0);
-      W3[2] = sqrt(6.0) / 3.0;
-#endif
-
-      getmat(Wmat, W0, W1, W2, W3);
-      getQR(Q, R, Wmat);
-      getinv(Rinv, R);
-
-      weight[0] = (fabs(Rinv[0][0]) > 1E-10) ? Rinv[0][0] : 0.0;
-      weight[1] = (fabs(Rinv[0][1]) > 1E-10) ? Rinv[0][1] : 0.0;
-      weight[2] = (fabs(Rinv[0][2]) > 1E-10) ? Rinv[0][2] : 0.0;
-      weight[3] = (fabs(Rinv[1][1]) > 1E-10) ? Rinv[1][1] : 0.0;
-      weight[4] = (fabs(Rinv[1][2]) > 1E-10) ? Rinv[1][2] : 0.0;
-      weight[5] = (fabs(Rinv[2][2]) > 1E-10) ? Rinv[2][2] : 0.0;
-
-      weight[6] = (fabs(Q[0][0]) > 1E-10) ? Q[0][0] : 0.0;
-      weight[7] = (fabs(Q[0][1]) > 1E-10) ? Q[0][1] : 0.0;
-      weight[8] = (fabs(Q[0][2]) > 1E-10) ? Q[0][2] : 0.0;
-      weight[9] = (fabs(Q[1][0]) > 1E-10) ? Q[1][0] : 0.0;
-      weight[10] = (fabs(Q[1][1]) > 1E-10) ? Q[1][1] : 0.0;
-      weight[11] = (fabs(Q[1][2]) > 1E-10) ? Q[1][2] : 0.0;
-      weight[12] = (fabs(Q[2][0]) > 1E-10) ? Q[2][0] : 0.0;
-      weight[13] = (fabs(Q[2][1]) > 1E-10) ? Q[2][1] : 0.0;
-      weight[14] = (fabs(Q[2][2]) > 1E-10) ? Q[2][2] : 0.0;
-
-      weight += 16;
-    }
-  }
-#endif
+  int cg_iter;
+  bool valid;
 
   mHess.initialize( pd, err );  //hMesh(mesh);
-  if (!func.update( pd, obj, mGrad, err ) && MSQ_CHKERR(err)) { //if (gFcn(&obj, mesh)) {
-    return ; //  fprintf(stderr, "Invalid starting point.\n");
-             //  exit(-1);
+  valid = func.update( pd, obj, mGrad, mHess, err ); MSQ_ERRRTN(err);
+  if (!valid) {
+    MSQ_SETERR(err)("Initial objective function is not valid", MsqError::INVALID_MESH);
+    return;
   }
+  compute_preconditioner( err ); MSQ_ERRRTN(err);
+  pd.recreate_vertices_memento( mMemento, err ); MSQ_ERRRTN(err);
 
-  //prec = preCreate(precond, nn, mesh->nz);
-
-  //v  = (double *)malloc(3*sizeof(double)*nn);
   wVect.resize(nn); w = &wVect[0];//w  = (double *)malloc(3*sizeof(double)*nn);
   zVect.resize(nn); z = &zVect[0];//z  = (double *)malloc(3*sizeof(double)*nn);
   dVect.resize(nn); d = &dVect[0];//d  = (double *)malloc(3*sizeof(double)*nn);
   pVect.resize(nn); p = &pVect[0];//p  = (double *)malloc(3*sizeof(double)*nn);
   rVect.resize(nn); r = &rVect[0];//r  = (double *)malloc(3*sizeof(double)*nn);
 
-  //gatherMesh(v, mesh);
   norm_r = norm(&mGrad[0], nn);
   norm_g = sqrt(norm_r);
 
-  //getrusage(RUSAGE_SELF, &r1);
-
-  //m1 = (double) r1.ru_utime.tv_usec;
-  //m2 = (double) r0.ru_utime.tv_usec;
-  //m = m1 - m2;
-    
-  //s1 = (double) r1.ru_utime.tv_sec;
-  //s2 = (double) r0.ru_utime.tv_sec;
-  //s = s1 - s2;
-
-  //t1 = s + m / MICROSEC;
-
-  //m1 = (double) r1.ru_stime.tv_usec;
-  //m2 = (double) r0.ru_stime.tv_usec;
-  //m = m1 - m2;
-
-  //s1 = (double) r1.ru_stime.tv_sec;
-  //s2 = (double) r0.ru_stime.tv_sec;
-  //s = s1 - s2;
-
-  //t2 = s + m / MICROSEC;
-
-#ifdef DISPLAY_MAX
-  //oMax(&fmax, &fidx, mesh);
-  //printf("%4d I      %10.9e %5.4e            %5.4e %5d "
-  //       "usr: %5.4e sys: %5.4e tot: %5.4e\n",
-  //       iter, obj, norm_g, fmax, fidx, t1, t2, t1+t2);
-#else
-  //printf("%4d I      %10.9e %5.4e            "
-  //       "usr: %5.4e sys: %5.4e tot: %5.4e\n",
-  //       iter, obj, norm_g, t1, t2, t1+t2);
-#endif
-
   while (!term.terminate() && (radius > 1e-20)) {
-    pd.recreate_vertices_memento( mMemento, err ); MSQ_ERRRTN(err);
-
-    if (needH) {
-      //getrusage(RUSAGE_SELF, &r0);
-      //++iter;
-
-      //++nh;
-      func.update( pd, obj, mGrad, mHess, err ); MSQ_ERRRTN(err); //hOnly(mesh);
-      compute_preconditioner( err ); MSQ_ERRRTN(err); //prec->calc(prec, mesh);
-      needH = 0;
-    }
 
     memset(d, 0, 3*sizeof(double)*nn);
     memcpy(r, &mGrad[0], nn*sizeof(Vector3D)); //memcpy(r, mesh->g, 3*sizeof(double)*nn);
@@ -410,122 +260,51 @@ void TrustRegion::optimize_vertex_positions( PatchData& pd, MsqError& err )
     kappa = alpha + beta;
 
     /* Put the new point into the locations */
-    //axpy(w, v, 1.0, d, nn);
-    //scatterMesh(mesh, w);
     pd.move_free_vertices_constrained( d, nn, 1.0, err ); MSQ_ERRRTN(err);
 
-    ++nf;
-    ferr = func.evaluate( pd, objn, err ); MSQ_ERRRTN(err); //ferr = oFcn(&objn, mesh);
-
-#ifdef OUTPUT_STATS
-    printf("alpha = %20.19e\n", alpha);
-    printf("beta  = %20.19e\n", beta);
-    printf("kappa = %20.19e\n", kappa);
-
-    if (!ferr) {
-      printf("new   = %20.19e\n", objn);
-      printf("old   = %20.19e\n", obj);
-      printf("ared  = %20.19e\n", objn - obj);
-      printf("pred  = %20.19e\n", kappa);
-      printf("ratio = %20.19e\n", (objn - obj) / kappa);
-    }
-#endif
-
-    if (ferr) {
-      if ((fabs(kappa) <= tr_num_tol) && (fabs(objn - obj) <= tr_num_tol)) {
-	kappa = 1;
-      }
-      else {
-	kappa = (objn - obj) / kappa;
-      }
-
-      if (kappa >= eta_1) {
-	/* Iterate is acceptable */
-
-        if (kappa >= eta_2) {
-	  /* Iterate is a very good step, increase radius */
-	  radius *= tr_incr;
-	  if (radius > 1e20) {
-	    radius = 1e20;
-	  }
-	}
-
-	/* Update the iterate (v = current point, w = new point) so swap */
-	//t = v;
-	//v = w;
-	//w = t;
-
-	++ng;
-	obj = objn;
-	func.evaluate( pd, objn, mGrad, err ); //gOnly(mesh);
-	needH = 1;
-      }
-      else {
-	/* Iterate is unacceptable */
-	radius *= tr_decr_def;
-        pd.set_to_vertices_memento( mMemento, err ); MSQ_ERRRTN(err); //scatterMesh(mesh, v);
-      }
-    }
-    else {
+    valid = func.evaluate( pd, objn, err ); MSQ_ERRRTN(err); 
+    if (!valid) {
       /* Function not defined at trial point */
       radius *= tr_decr_undef;
-      pd.set_to_vertices_memento( mMemento, err ); MSQ_ERRRTN(err); //scatterMesh(mesh, v);
+      pd.set_to_vertices_memento( mMemento, err ); MSQ_ERRRTN(err); 
+      continue;
     }
+      
+
+    if ((fabs(kappa) <= tr_num_tol) && (fabs(objn - obj) <= tr_num_tol)) {
+      kappa = 1;
+    }
+    else {
+      kappa = (objn - obj) / kappa;
+    }
+    
+    if (kappa < eta_1) {
+      /* Iterate is unacceptable */
+      radius *= tr_decr_def;
+      pd.set_to_vertices_memento( mMemento, err ); MSQ_ERRRTN(err); 
+      continue;
+    }
+
+      /* Iterate is acceptable */
+    if (kappa >= eta_2) {
+      /* Iterate is a very good step, increase radius */
+      radius *= tr_incr;
+      if (radius > 1e20) {
+	radius = 1e20;
+      }
+    }
+
+    func.update( pd, obj, mGrad, mHess, err );
+    compute_preconditioner( err ); MSQ_ERRRTN(err);
+    pd.recreate_vertices_memento( mMemento, err ); MSQ_ERRRTN(err);
 
     norm_r = norm(&mGrad[0], nn);
     norm_g = sqrt(norm_r);
-
-    //if (needH) {
-      //getrusage(RUSAGE_SELF, &r1);
-
-      //m1 = (double) r1.ru_utime.tv_usec;
-      //m2 = (double) r0.ru_utime.tv_usec;
-      //m = m1 - m2;
-    
-      //s1 = (double) r1.ru_utime.tv_sec;
-      //s2 = (double) r0.ru_utime.tv_sec;
-      //s = s1 - s2;
-      
-      //t1 = s + m / MICROSEC;
-      
-      //m1 = (double) r1.ru_stime.tv_usec;
-      //m2 = (double) r0.ru_stime.tv_usec;
-      //m = m1 - m2;
-    
-      //s1 = (double) r1.ru_stime.tv_sec;
-      //s2 = (double) r0.ru_stime.tv_sec;
-      //s = s1 - s2;
-      
-      //t2 = s + m / MICROSEC;
-      
-#ifdef DISPLAY_MAX
-      //oMax(&fmax, &fidx, mesh);
-      //printf("%4d N %4d %10.9e %5.4e %5.4e %5.4e %5d "
-      //       "usr: %5.4e sys: %5.4e tot: %5.4e\n",
-      //       iter, cg_iter, obj, norm_g, radius, fmax, fidx, t1, t2, t1+t2);
-#else
-      //printf("%4d N %4d %10.9e %5.4e %5.4e usr: %5.4e sys: %5.4e tot: %5.4e\n",
-      //       iter, cg_iter, obj, norm_g, radius, t1, t2, t1+t2);
-#endif
-    //}
 
     // checks stopping criterion 
     term.accumulate_patch( pd, err ); MSQ_ERRRTN(err);
     term.accumulate_inner( pd, objn, &mGrad[0], err ); MSQ_ERRRTN(err);
   }
-
-  //printf("Function evals: %4d\nGradient evals: %4d\nHessian  evals: %4d\n", 
-  //	 nf, ng, nh);
-
-  //prec->destroy(prec);
-
-  //free(v);
-  //free(w);
-  //free(z);
-  //free(d);
-  //free(p);
-  //free(r);
-  //return 0;
 }    
 
 } // namespace Mesquite
