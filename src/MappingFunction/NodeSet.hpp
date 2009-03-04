d7e013dafdd81b07af333ba770f3d5e00ec3e83d
Jason A. Kraftcheck <kraftche@cae.wisc.edu>
2009-03-04 19:57:55 +0000

    We currently use bits in an unsigned integer to specify which higher-order nodes are present in an element, and will need something similar to specify logical sample points within an element (such as at middle of edge 1).  Add a wapper class for this type of thing that so that code is cleaner and whole paragraphs of documentation aren't required to explain the meaning of a single function argument.

---
+++ src/MappingFunction/NodeSet.hpp
@@ -0,0 +1,295 @@
+/* ***************************************************************** 
+    MESQUITE -- The Mesh Quality Improvement Toolkit
+
+    Copyright 2007 Sandia National Laboratories.  Developed at the
+    University of Wisconsin--Madison under SNL contract number
+    624796.  The U.S. Government and the University of Wisconsin
+    retain certain rights to this software.
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License 
+    (lgpl.txt) along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    (2009) kraftche@cae.wisc.edu    
+
+  ***************************************************************** */
+
+
+/** \file NodeSet.hpp
+ *  \brief 
+ *  \author Jason Kraftcheck 
+ */
+
+#ifndef MSQ_NODE_SET_HPP
+#define MSQ_NODE_SET_HPP
+
+#include "Mesquite.hpp"
+#include <assert.h>
+#include <iosfwd>
+#include "Bits.hpp"
+
+namespace Mesquite {
+
+/** Utility class for storing one boolean mark/flag for each node in an element */
+class NodeSet {
+  private:
+    typedef unsigned BitSet;
+    BitSet bits; //!< The data, one bit for each possible node location
+    
+    //! Return a bool value indicating the state of the specified bit.
+    inline bool bit_to_bool( unsigned num ) const
+      { return static_cast<bool>( (bits >> num) & 1u ); }
+    
+    //! Set the specified bit to 1
+    inline void set_bit( unsigned num )
+      { bits |= (1u << num); }
+    
+    //! Clear the specified bit (set it to zero)
+    inline void clear_bit( unsigned num )
+      { bits &= ~(1u << num); }
+    
+    //! Count the number of non-zero bits with less significance than
+    //! the specified bit.  (Mask out all bits except those of before
+    //! the specified position and then count the remaining bits.)
+    unsigned num_before_bit( unsigned position ) const
+      { return popcount(bits & ~((~0u) << position)); }
+  
+  public:
+    
+    //! Misc constants.  Only NUM_CORNER_BITS, NUM_EDGE_BITS, and
+    //! NUM_REGION_BITS should be modified.  All other contants are
+    //! a function of those three values and the size of the bit storage.
+    enum { 
+      NUM_TOTAL_BITS = 8*sizeof(BitSet),
+      MSB_POS = NUM_TOTAL_BITS - 1,
+      //! Maximum number of corner nodes.
+      NUM_CORNER_BITS = 8, 
+      //! Maximum number of mid-edge nodes
+      NUM_EDGE_BITS = 16,  
+      //! Maximum number of mid-volume nodes
+      NUM_REGION_BITS = 1, 
+      //! Maximum number of mid-face nodes
+      NUM_FACE_BITS = NUM_TOTAL_BITS - (NUM_CORNER_BITS + NUM_EDGE_BITS + NUM_REGION_BITS),   
+      
+      //! LSB of corner node storage
+      CORNER_OFFSET = 0,
+      //! LSB of mid-edgee storage
+      EDGE_OFFSET = CORNER_OFFSET + NUM_CORNER_BITS,
+      //! LSB of mid-face storage
+      FACE_OFFSET =   EDGE_OFFSET + NUM_EDGE_BITS,
+      //! LSB of mid-region storage
+      REGION_OFFSET = FACE_OFFSET + NUM_FACE_BITS,
+    
+      //! Bit mask for all non-corner bits
+      MID_NODE_MASK = (~0u) << NUM_CORNER_BITS,
+      //! Bit mask for all corner bits
+      CORNER_MASK = ~MID_NODE_MASK,
+      //! Mid-region mask
+      REGION_MASK = ~((~0u) >> NUM_REGION_BITS),
+      //! Bit mask for all mid-edge nodes
+      EDGE_MASK = (MID_NODE_MASK << (NUM_TOTAL_BITS - EDGE_OFFSET - NUM_EDGE_BITS)) >> (NUM_TOTAL_BITS - EDGE_OFFSET - NUM_EDGE_BITS),
+      //! Bit mask for all mid-face nodes
+      FACE_MASK = ~(CORNER_MASK|EDGE_MASK|REGION_MASK),
+    };
+    
+    NodeSet() : bits(0u) {}
+    
+    //! Clear all values
+    void clear() { bits = 0u; }
+    
+    //! Set all marks/flags
+    void set_all_nodes() { bits = ~0u; }
+    
+    //! Number of marked/flagged nodes
+    unsigned num_nodes() const
+      { return popcount(bits); }
+      
+    //! Check if any mid-nodes (higher-order nodes) are flaged
+    BitSet have_any_mid_node() const
+      { return bits & MID_NODE_MASK; }
+    //! Check if any corner nodes are present
+    BitSet have_any_corner_node() const
+      { return bits & CORNER_MASK; }
+    //! Check if any mid-edge nodes are present
+    BitSet have_any_mid_edge_node() const
+      { return bits & EDGE_MASK; }
+    //! Check if any mid-face nodes are present
+    BitSet have_any_mid_face_node() const
+      { return bits & FACE_MASK; }
+    //! Check if any mid-region nodes are present
+    BitSet have_any_mid_region_node() const
+      { return bits & REGION_MASK; }
+      
+    //! Set all mid-nodes
+    void set_all_mid_nodes()
+      { bits |= MID_NODE_MASK; }
+    //! Set all corner nodes
+    void set_all_corner_nodes()
+      { bits |= CORNER_MASK; }
+    //! Set all mid-edge nodes
+    void set_all_mid_edge_nodes()
+      { bits |= EDGE_MASK; }
+    //! Set all mid-face nodes
+    void set_all_mid_face_nodes()
+      { bits |= FACE_MASK; }
+    //! Set all mid-region nodes
+    void set_all_mid_region_nodes()
+      { bits |= REGION_MASK; }
+      
+    //! Clear all mid-nodes
+    void clear_all_mid_nodes()
+      { bits &= ~MID_NODE_MASK; }
+    //! Clear all corner nodes
+    void clear_all_corner_nodes()
+      { bits &= ~CORNER_MASK; }
+    //! Clear all mid-edge nodes
+    void clear_all_mid_edge_nodes()
+      { bits &= ~EDGE_MASK; }
+    //! Clear all mid-face nodes
+    void clear_all_mid_face_nodes()
+      { bits &= ~FACE_MASK; }
+    //! Clear all mid-region nodes
+    void clear_all_mid_region_nodes()
+      { bits &= ~REGION_MASK; }
+    
+    //! Position of a corner node in the list
+    static unsigned corner_node_position( unsigned num ) 
+      { assert(num < NUM_CORNER_BITS); return num + CORNER_OFFSET; }
+    //! Position of a mid-edge node in the list
+    static unsigned mid_edge_node_position( unsigned num ) 
+      { assert(num < NUM_EDGE_BITS); return num + EDGE_OFFSET; }
+    //! Position of a mid-face node in the list
+    static unsigned mid_face_node_position( unsigned num ) 
+      { assert(num < NUM_FACE_BITS); return num + FACE_OFFSET; }
+    //! Position of a mid-region node in the list
+    static unsigned mid_region_node_position( unsigned num = 0 ) 
+      { assert(num < NUM_REGION_BITS); return num + REGION_OFFSET; }
+    //! Position of a node in the list
+    static unsigned position( unsigned dim, unsigned num ) {
+      switch (dim) { 
+        case 0: return corner_node_position(num); 
+        case 1: return mid_edge_node_position(num);
+        case 2: return mid_face_node_position(num);
+        case 3: return mid_region_node_position(num);
+#ifndef NDEBUG
+        default: assert(0); return ~0u;
+#endif
+      }
+    }
+    
+    //! Mark/flag corner node
+    void set_corner_node( unsigned num ) 
+      { set_bit( corner_node_position(num) ); }
+    //! Mark/flag mid-edge node
+    void set_mid_edge_node( unsigned num ) 
+      { set_bit( mid_edge_node_position(num) ); }
+    //! Mark/flag mid-face node
+    void set_mid_face_node( unsigned num ) 
+      { set_bit( mid_face_node_position(num) ); }
+    //! Mark/flag mid-region node
+    void set_mid_region_node( unsigned num = 0 ) 
+      { set_bit( mid_region_node_position(num) ); }
+    //! Mark/flag node
+    void set_node( unsigned dim, unsigned num ) {
+      switch (dim) { 
+        case 0: set_corner_node(num); break;
+        case 1: set_mid_edge_node(num); break;
+        case 2: set_mid_face_node(num); break;
+        case 3: set_mid_region_node(num); break;
+        default: assert(0);
+      }
+    }
+    
+    //! un-mark (clear flag for) corner node
+    void clear_corner_node( unsigned num ) 
+      { clear_bit( corner_node_position(num) ); }
+    //! un-mark (clear flag for) mid-edge node
+    void clear_mid_edge_node( unsigned num ) 
+      { clear_bit( mid_edge_node_position(num) ); }
+    //! un-mark (clear flag for) mid-face node
+    void clear_mid_face_node( unsigned num ) 
+      { clear_bit( mid_face_node_position(num) ); }
+    //! un-mark (clear flag for) mid-region node
+    void clear_mid_region_node( unsigned num = 0 ) 
+      { clear_bit( mid_region_node_position(num) ); }
+    //! un-mark (clear flag for) node
+    void clear_node( unsigned dim, unsigned num ) {
+      switch (dim) { 
+        case 0: clear_corner_node(num); break;
+        case 1: clear_mid_edge_node(num); break;
+        case 2: clear_mid_face_node(num); break;
+        case 3: clear_mid_region_node(num); break;
+        default: assert(0);
+      }
+    }
+    
+    //! Get mark/flag for corner node
+    bool corner_node( unsigned num ) const
+      { return bit_to_bool( corner_node_position(num) ); }
+    //! Get mark/flag for mid-edge node
+    bool mid_edge_node( unsigned num ) const
+      { return bit_to_bool( mid_edge_node_position(num) ); }
+    //! Get mark/flag for mid-face node
+    bool mid_face_node( unsigned num ) const
+      { return bit_to_bool( mid_face_node_position(num) ); }
+    //! Get mark/flag for mid-region node
+    bool mid_region_node( unsigned num = 0 ) const
+      {  return bit_to_bool( mid_region_node_position(num) ); }
+    //! Get mark/flag for node
+    bool node( unsigned dim, unsigned num ) const {
+      switch (dim) { 
+        case 0: return corner_node(num); break;
+        case 1: return mid_edge_node(num); break;
+        case 2: return mid_face_node(num); break;
+        case 3: return mid_region_node(num); break;
+#ifndef NDEBUG
+        default: assert(0); return false;
+#endif
+      }
+    }
+
+    //! Get number of marked/flagged nodes preceeding a specified corner node in the list
+    unsigned num_before_corner( unsigned num ) const 
+      { return num_before_bit( corner_node_position(num) ); } 
+    //! Get number of marked/flagged nodes preceeding a specified mid-edge node in the list
+    unsigned num_before_mid_edge( unsigned num ) const 
+      { return num_before_bit( mid_edge_node_position(num) ); } 
+    //! Get number of marked/flagged nodes preceeding a specified mid-face node in the list
+    unsigned num_before_mid_face( unsigned num ) const 
+      { return num_before_bit( mid_face_node_position(num) ); } 
+    //! Get number of marked/flagged nodes preceeding a specified mid-region node in the list
+    unsigned num_before_mid_region( unsigned num ) const 
+      { return num_before_bit( mid_region_node_position(num) ); } 
+    //! Get number of marked/flagged nodes preceeding a specified node in the list
+    unsigned num_before( unsigned dim, unsigned num ) const {
+      switch (dim) { 
+        case 0: return num_before_corner(num); break;
+        case 1: return num_before_mid_edge(num); break;
+        case 2: return num_before_mid_face(num); break;
+        case 3: return num_before_mid_region(num); break;
+#ifndef NDEBUG
+        default: assert(0); return ~0u;
+#endif
+      }
+    }
+    
+    unsigned get_bits() const { return bits; }
+};
+
+//! Print bits in reverse order (least-signficant to most-significant,
+//! or corner 0 to mid-region).
+msq_stdio::ostream& operator<<( msq_stdio::ostream& s, NodeSet set );
+
+} // namespace Mesquite
+
+#endif
---
+++ testSuite/unit/NodeSetTest.cpp
@@ -0,0 +1,376 @@
+/* ***************************************************************** 
+    MESQUITE -- The Mesh Quality Improvement Toolkit
+
+    Copyright 2006 Sandia National Laboratories.  Developed at the
+    University of Wisconsin--Madison under SNL contract number
+    624796.  The U.S. Government and the University of Wisconsin
+    retian certain rights to this software.
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License 
+    (lgpl.txt) along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+    (2006) kraftche@cae.wisc.edu
+   
+  ***************************************************************** */
+
+
+/** \file NodeSetTest.cpp
+ *  \brief 
+ *  \author Jason Kraftcheck 
+ */
+
+#include "Mesquite.hpp"
+#include "NodeSet.hpp"
+#include "UnitUtil.hpp"
+
+using namespace Mesquite;
+using namespace std;
+
+class NodeSetTest : public CppUnit::TestFixture
+{
+  private:
+    CPPUNIT_TEST_SUITE( NodeSetTest );
+    CPPUNIT_TEST(test_init);
+    CPPUNIT_TEST(test_clear);
+    CPPUNIT_TEST(test_set_get_clear_simple);
+    CPPUNIT_TEST(test_set_get_clear_dim);
+    CPPUNIT_TEST(test_set_node);
+    CPPUNIT_TEST(test_clear_node);
+    CPPUNIT_TEST(test_num_nodes);
+    CPPUNIT_TEST(test_have_any);
+    CPPUNIT_TEST(test_set_all);
+    CPPUNIT_TEST(test_clear_all);
+    CPPUNIT_TEST(test_num_before);
+    CPPUNIT_TEST_SUITE_END();
+    
+  public:
+
+    void test_init();
+    void test_clear();
+    void test_set_get_clear_simple();
+    void test_set_get_clear_dim();
+    void test_set_node();
+    void test_clear_node();
+    void test_num_nodes();
+    void test_have_any();
+    void test_set_all();
+    void test_clear_all();
+    void test_num_before();
+};
+
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(NodeSetTest, "NodeSetTest");
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(NodeSetTest, "Unit");
+
+void NodeSetTest::test_init()
+{
+  NodeSet set;
+  CPPUNIT_ASSERT( !set.get_bits() );
+}
+
+void NodeSetTest::test_clear()
+{
+  NodeSet set;
+  set.set_all_corner_nodes();
+  set.clear();
+  CPPUNIT_ASSERT( !set.get_bits() );
+  set.set_all_mid_edge_nodes();
+  set.clear();
+  CPPUNIT_ASSERT( !set.get_bits() );
+  set.set_all_mid_nodes();
+  set.clear();
+  CPPUNIT_ASSERT( !set.get_bits() );
+}
+
+void NodeSetTest::test_set_get_clear_simple()
+{
+  NodeSet set;
+  for (unsigned i = 0; i < NodeSet::NUM_CORNER_BITS; ++i) {
+    CPPUNIT_ASSERT( !set.corner_node(i) );
+    set.set_corner_node( i );
+    CPPUNIT_ASSERT( set.corner_node(i) );
+    set.clear_corner_node( i );
+    CPPUNIT_ASSERT( !set.corner_node(i) );
+  }
+  for (unsigned i = 0; i < NodeSet::NUM_EDGE_BITS; ++i) {
+    CPPUNIT_ASSERT( !set.mid_edge_node(i) );
+    set.set_mid_edge_node( i );
+    CPPUNIT_ASSERT( set.mid_edge_node(i) );
+    set.clear_mid_edge_node( i );
+    CPPUNIT_ASSERT( !set.mid_edge_node(i) );
+  }
+  for (unsigned i = 0; i < NodeSet::NUM_FACE_BITS; ++i) {
+    CPPUNIT_ASSERT( !set.mid_face_node(i) );
+    set.set_mid_face_node( i );
+    CPPUNIT_ASSERT( set.mid_face_node(i) );
+    set.clear_mid_face_node( i );
+    CPPUNIT_ASSERT( !set.mid_face_node(i) );
+  }
+  for (unsigned i = 0; i < NodeSet::NUM_REGION_BITS; ++i) {
+    CPPUNIT_ASSERT( !set.mid_region_node(i) );
+    set.set_mid_region_node( i );
+    CPPUNIT_ASSERT( set.mid_region_node(i) );
+    set.clear_mid_region_node( i );
+    CPPUNIT_ASSERT( !set.mid_region_node(i) );
+  }
+}
+
+void NodeSetTest::test_set_get_clear_dim()
+{
+  NodeSet set;
+  for (unsigned i = 0; i < NodeSet::NUM_CORNER_BITS; ++i) {
+    CPPUNIT_ASSERT( !set.node(0,i) );
+    set.set_node( 0, i );
+    CPPUNIT_ASSERT( set.node(0,i) );
+    set.clear_node( 0, i );
+    CPPUNIT_ASSERT( !set.node(0,i) );
+  }
+  for (unsigned i = 0; i < NodeSet::NUM_EDGE_BITS; ++i) {
+    CPPUNIT_ASSERT( !set.node(1,i) );
+    set.set_node( 1, i );
+    CPPUNIT_ASSERT( set.node(1,i) );
+    set.clear_node( 1, i );
+    CPPUNIT_ASSERT( !set.node(1,i) );
+  }
+  for (unsigned i = 0; i < NodeSet::NUM_FACE_BITS; ++i) {
+    CPPUNIT_ASSERT( !set.node(2,i) );
+    set.set_node( 2, i );
+    CPPUNIT_ASSERT( set.node(2,i) );
+    set.clear_node( 2, i );
+    CPPUNIT_ASSERT( !set.node(2,i) );
+  }
+  for (unsigned i = 0; i < NodeSet::NUM_REGION_BITS; ++i) {
+    CPPUNIT_ASSERT( !set.node(3,i) );
+    set.set_node( 3, i );
+    CPPUNIT_ASSERT( set.node(3,i) );
+    set.clear_node( 3, i );
+    CPPUNIT_ASSERT( !set.node(3,i) );
+  }
+}
+
+void NodeSetTest::test_set_node()
+{
+  NodeSet set;
+  for (unsigned i = 0; i < NodeSet::NUM_CORNER_BITS; ++i) {
+    set.set_corner_node( i );
+    CPPUNIT_ASSERT_EQUAL( 1u << (NodeSet::CORNER_OFFSET + i), set.get_bits() );
+    set.clear();
+  }
+  for (unsigned i = 0; i < NodeSet::NUM_EDGE_BITS; ++i) {
+    set.set_mid_edge_node( i );
+    CPPUNIT_ASSERT_EQUAL( 1u << (NodeSet::EDGE_OFFSET + i), set.get_bits() );
+    set.clear();
+  }
+  for (unsigned i = 0; i < NodeSet::NUM_FACE_BITS; ++i) {
+    set.set_mid_face_node( i );
+    CPPUNIT_ASSERT_EQUAL( 1u << (NodeSet::FACE_OFFSET + i), set.get_bits() );
+    set.clear();
+  }
+  for (unsigned i = 0; i < NodeSet::NUM_REGION_BITS; ++i) {
+    set.set_mid_region_node( i );
+    CPPUNIT_ASSERT_EQUAL( 1u << (NodeSet::REGION_OFFSET + i), set.get_bits() );
+    set.clear();
+  }
+}
+
+void NodeSetTest::test_clear_node()
+{
+  NodeSet set;
+  for (unsigned i = 0; i < NodeSet::NUM_CORNER_BITS; ++i) {
+    set.set_all_nodes();
+    set.clear_corner_node( i );
+    CPPUNIT_ASSERT_EQUAL( ~(1u << (NodeSet::CORNER_OFFSET + i)), set.get_bits() );
+  }
+  for (unsigned i = 0; i < NodeSet::NUM_EDGE_BITS; ++i) {
+    set.set_all_nodes();
+    set.clear_mid_edge_node( i );
+    CPPUNIT_ASSERT_EQUAL( ~(1u << (NodeSet::EDGE_OFFSET + i)), set.get_bits() );
+  }
+  for (unsigned i = 0; i < NodeSet::NUM_FACE_BITS; ++i) {
+    set.set_all_nodes();
+    set.clear_mid_face_node( i );
+    CPPUNIT_ASSERT_EQUAL( ~(1u << (NodeSet::FACE_OFFSET + i)), set.get_bits() );
+  }
+  for (unsigned i = 0; i < NodeSet::NUM_REGION_BITS; ++i) {
+    set.set_all_nodes();
+    set.clear_mid_region_node( i );
+    CPPUNIT_ASSERT_EQUAL( ~(1u << (NodeSet::REGION_OFFSET + i)), set.get_bits() );
+  }
+}
+
+void NodeSetTest::test_num_nodes()
+{
+  NodeSet set;
+  set.clear();
+  set.set_corner_node(1);
+  CPPUNIT_ASSERT_EQUAL( 1u, set.num_nodes() );
+  set.set_corner_node(3);
+  CPPUNIT_ASSERT_EQUAL( 2u, set.num_nodes() );
+  set.set_mid_region_node(0);
+  CPPUNIT_ASSERT_EQUAL( 3u, set.num_nodes() );
+  set.set_mid_edge_node(1);
+  set.set_mid_edge_node(2);
+  CPPUNIT_ASSERT_EQUAL( 5u, set.num_nodes() );
+  set.set_all_nodes();
+  CPPUNIT_ASSERT_EQUAL( (unsigned)NodeSet::NUM_TOTAL_BITS, set.num_nodes() );
+}
+
+void NodeSetTest::test_have_any()
+{
+  NodeSet set;
+  set.clear();
+  
+  CPPUNIT_ASSERT( !set.have_any_corner_node() );
+  set.set_corner_node(0);
+  CPPUNIT_ASSERT( set.have_any_corner_node() );
+  set.clear_corner_node(0);
+  CPPUNIT_ASSERT( !set.have_any_corner_node() );
+  set.set_corner_node( NodeSet::NUM_CORNER_BITS-1 );
+  CPPUNIT_ASSERT( set.have_any_corner_node() );
+  CPPUNIT_ASSERT( !set.have_any_mid_node() );
+
+  CPPUNIT_ASSERT( !set.have_any_mid_edge_node() );
+  set.set_mid_edge_node(0);
+  CPPUNIT_ASSERT( set.have_any_mid_edge_node() );
+  CPPUNIT_ASSERT( set.have_any_mid_node() );
+  set.clear_mid_edge_node(0);
+  CPPUNIT_ASSERT( !set.have_any_mid_edge_node() );
+  CPPUNIT_ASSERT( !set.have_any_mid_node() );
+  set.set_mid_edge_node( NodeSet::NUM_EDGE_BITS-1 );
+  CPPUNIT_ASSERT( set.have_any_mid_edge_node() );
+  CPPUNIT_ASSERT( set.have_any_mid_node() );
+  set.clear();
+
+  CPPUNIT_ASSERT( !set.have_any_mid_face_node() );
+  set.set_mid_face_node(0);
+  CPPUNIT_ASSERT( set.have_any_mid_face_node() );
+  CPPUNIT_ASSERT( set.have_any_mid_node() );
+  set.clear_mid_face_node(0);
+  CPPUNIT_ASSERT( !set.have_any_mid_face_node() );
+  CPPUNIT_ASSERT( !set.have_any_mid_node() );
+  set.set_mid_face_node( NodeSet::NUM_FACE_BITS-1 );
+  CPPUNIT_ASSERT( set.have_any_mid_face_node() );
+  CPPUNIT_ASSERT( set.have_any_mid_node() );
+  set.clear();
+
+  CPPUNIT_ASSERT( !set.have_any_mid_region_node() );
+  set.set_mid_region_node(0);
+  CPPUNIT_ASSERT( set.have_any_mid_region_node() );
+  CPPUNIT_ASSERT( set.have_any_mid_node() );
+  set.clear_mid_region_node(0);
+  CPPUNIT_ASSERT( !set.have_any_mid_region_node() );
+  CPPUNIT_ASSERT( !set.have_any_mid_node() );
+  set.set_mid_region_node( NodeSet::NUM_REGION_BITS-1 );
+  CPPUNIT_ASSERT( set.have_any_mid_region_node() );
+  CPPUNIT_ASSERT( set.have_any_mid_node() );
+  set.clear();
+}
+
+static bool check_all( NodeSet set, unsigned dim, bool value )
+{
+  unsigned count = 0;
+  switch (dim) { case 0: count = NodeSet::NUM_CORNER_BITS; break;
+                 case 1: count = NodeSet::NUM_EDGE_BITS; break;
+                 case 2: count = NodeSet::NUM_FACE_BITS; break;
+                 case 3: count = NodeSet::NUM_REGION_BITS; break;
+                 default: CPPUNIT_ASSERT(false); }
+  for (unsigned i = 0; i < count; ++i)
+    if (set.node( dim, i ) != value )
+      return false;
+  return true;
+}
+  
+void NodeSetTest::test_set_all()
+{
+  NodeSet set;
+  set.set_all_corner_nodes();
+  CPPUNIT_ASSERT( check_all( set, 0, true ) );
+  CPPUNIT_ASSERT( check_all( set, 1, false ) );
+  CPPUNIT_ASSERT( check_all( set, 2, false ) );
+  CPPUNIT_ASSERT( check_all( set, 3, false ) );
+  set.clear();
+  set.set_all_mid_edge_nodes();
+  CPPUNIT_ASSERT( check_all( set, 0, false ) );
+  CPPUNIT_ASSERT( check_all( set, 1, true ) );
+  CPPUNIT_ASSERT( check_all( set, 2, false ) );
+  CPPUNIT_ASSERT( check_all( set, 3, false ) );
+  set.clear();
+  set.set_all_mid_face_nodes();
+  CPPUNIT_ASSERT( check_all( set, 0, false ) );
+  CPPUNIT_ASSERT( check_all( set, 1, false ) );
+  CPPUNIT_ASSERT( check_all( set, 2, true ) );
+  CPPUNIT_ASSERT( check_all( set, 3, false ) );
+  set.clear();
+  set.set_all_mid_region_nodes();
+  CPPUNIT_ASSERT( check_all( set, 0, false ) );
+  CPPUNIT_ASSERT( check_all( set, 1, false ) );
+  CPPUNIT_ASSERT( check_all( set, 2, false ) );
+  CPPUNIT_ASSERT( check_all( set, 3, true ) );
+}
+
+void NodeSetTest::test_clear_all()
+{
+  NodeSet set;
+  set.set_all_nodes();
+  set.clear_all_corner_nodes();
+  CPPUNIT_ASSERT( check_all( set, 0, false ) );
+  CPPUNIT_ASSERT( check_all( set, 1, true ) );
+  CPPUNIT_ASSERT( check_all( set, 2, true ) );
+  CPPUNIT_ASSERT( check_all( set, 3, true ) );
+  set.set_all_nodes();
+  set.clear_all_mid_edge_nodes();
+  CPPUNIT_ASSERT( check_all( set, 0, true ) );
+  CPPUNIT_ASSERT( check_all( set, 1, false ) );
+  CPPUNIT_ASSERT( check_all( set, 2, true ) );
+  CPPUNIT_ASSERT( check_all( set, 3, true ) );
+  set.set_all_nodes();
+  set.clear_all_mid_face_nodes();
+  CPPUNIT_ASSERT( check_all( set, 0, true ) );
+  CPPUNIT_ASSERT( check_all( set, 1, true ) );
+  CPPUNIT_ASSERT( check_all( set, 2, false ) );
+  CPPUNIT_ASSERT( check_all( set, 3, true ) );
+  set.set_all_nodes();
+  set.clear_all_mid_region_nodes();
+  CPPUNIT_ASSERT( check_all( set, 0, true ) );
+  CPPUNIT_ASSERT( check_all( set, 1, true ) );
+  CPPUNIT_ASSERT( check_all( set, 2, true ) );
+  CPPUNIT_ASSERT( check_all( set, 3, false ) );
+}
+
+void NodeSetTest::test_num_before()
+{
+  NodeSet set;
+  set.clear();
+  set.set_mid_face_node( 2 );
+  CPPUNIT_ASSERT_EQUAL( 0u, set.num_before_mid_face( 2 ) );
+  CPPUNIT_ASSERT_EQUAL( 1u, set.num_before_mid_face( 3 ) );
+  set.set_corner_node( 0 );
+  CPPUNIT_ASSERT_EQUAL( 1u, set.num_before_mid_face( 2 ) );
+  CPPUNIT_ASSERT_EQUAL( 2u, set.num_before_mid_face( 3 ) );
+  CPPUNIT_ASSERT_EQUAL( 0u, set.num_before_corner( 0 ) );
+  CPPUNIT_ASSERT_EQUAL( 1u, set.num_before_corner( 1 ) );
+  set.set_all_corner_nodes();
+  CPPUNIT_ASSERT_EQUAL( (unsigned)NodeSet::NUM_CORNER_BITS, set.num_before_mid_edge(0) );
+  CPPUNIT_ASSERT_EQUAL( (unsigned)NodeSet::NUM_CORNER_BITS-1, set.num_before_corner(NodeSet::NUM_CORNER_BITS-1) );
+  CPPUNIT_ASSERT_EQUAL( 0u, set.num_before_corner(0) );
+  CPPUNIT_ASSERT_EQUAL( 1u, set.num_before_corner(1) );
+  CPPUNIT_ASSERT_EQUAL( 2u, set.num_before_corner(2) );
+  CPPUNIT_ASSERT_EQUAL( 3u, set.num_before_corner(3) );
+  CPPUNIT_ASSERT_EQUAL( 4u, set.num_before_corner(4) );
+  set.set_all_nodes();
+  CPPUNIT_ASSERT_EQUAL( (unsigned)NodeSet::NUM_TOTAL_BITS - 1, set.num_before_mid_region( NodeSet::NUM_REGION_BITS -1 ) );
+  set.clear_mid_edge_node( 0 );
+  CPPUNIT_ASSERT_EQUAL( (unsigned)NodeSet::NUM_TOTAL_BITS - 2, set.num_before_mid_region( NodeSet::NUM_REGION_BITS -1 ) );
+  set.clear_mid_edge_node( 1 );
+  CPPUNIT_ASSERT_EQUAL( (unsigned)NodeSet::NUM_TOTAL_BITS - 3, set.num_before_mid_region( NodeSet::NUM_REGION_BITS -1 ) );
+}
