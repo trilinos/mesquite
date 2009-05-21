58ddc7bf436ef0fb9a7bdd3eff278109034f9ad2
Jason A. Kraftcheck <kraftche@cae.wisc.edu>
2009-05-21 21:52:37 +0000

    analytic derivs for another 2D target metric

--- src/QualityMetric/TMP/2D/Target2DShapeSize.cpp
+++ src/QualityMetric/TMP/2D/Target2DShapeSize.cpp
@@ -33,12 +33,17 @@
 #include "Mesquite.hpp"
 #include "Target2DShapeSize.hpp"
 #include "MsqMatrix.hpp"
+#include "TMPDerivs.hpp"
 
 namespace MESQUITE_NS {
 
 msq_std::string Target2DShapeSize::get_name() const
   { return "ShapeSize"; }
 
+/** \f$ \mu(T) = |T|^2 - 2 \psi(T) + 2 \f$
+ *  \f$ \psi(T) = \sqrt{|T|^2 + 2 \tau \f$
+ *  \f$ \tau = det(T) \f$
+ */
 bool Target2DShapeSize::evaluate( const MsqMatrix<2,2>& A, 
                                   const MsqMatrix<2,2>& W, 
                                   double& result, 
@@ -51,5 +56,56 @@ bool Target2DShapeSize::evaluate( const MsqMatrix<2,2>& A,
 }
 
 
+bool Target2DShapeSize::evaluate_with_grad( const MsqMatrix<2,2>& A, 
+                                            const MsqMatrix<2,2>& W, 
+                                            double& result, 
+                                            MsqMatrix<2,2>& deriv_wrt_A,
+                                            MsqError& err )
+{
+  const MsqMatrix<2,2> Winv = inverse(W);
+  const MsqMatrix<2,2> T = A * Winv;
+  const double frob_sqr = sqr_Frobenius(T);
+  const double psi = sqrt( frob_sqr + 2.0*det(T) );
+  result = frob_sqr - 2.0 * psi + 2.0;
+
+  deriv_wrt_A = T;
+  deriv_wrt_A *= (1.0 - 1.0/psi);
+  deriv_wrt_A -= 1.0/psi * transpose_adj(T);
+  deriv_wrt_A *= 2;
+  deriv_wrt_A = deriv_wrt_A * transpose(Winv);
+  return true;
+}
+
+bool Target2DShapeSize::evaluate_with_hess( const MsqMatrix<2,2>& A, 
+                                            const MsqMatrix<2,2>& W, 
+                                            double& result, 
+                                            MsqMatrix<2,2>& deriv_wrt_A,
+                                            MsqMatrix<2,2> second[3],
+                                            MsqError& err )
+{
+  const MsqMatrix<2,2> Winv = inverse(W);
+  const MsqMatrix<2,2> T = A * Winv;
+  const double frob_sqr = sqr_Frobenius(T);
+  const double psi = sqrt( frob_sqr + 2.0*det(T) );
+  result = frob_sqr - 2.0 * psi + 2.0;
+
+  deriv_wrt_A = T;
+  deriv_wrt_A *= (1.0 - 1.0/psi);
+  deriv_wrt_A -= 1.0/psi * transpose_adj(T);
+  deriv_wrt_A *= 2;
+  deriv_wrt_A = deriv_wrt_A * transpose(Winv);
+  
+  const double s = T(0,1) - T(1,0);
+  const double tr = trace(T);
+  const double f = -2.0/(psi*psi*psi);
+  second[0](0,0) = second[1](0,1) = second[2](1,1) =  f*s*s;
+  second[0](0,1) = second[0](1,0) = second[1](1,1) = -f*s*tr;
+  second[1](0,0) = second[2](0,1) = second[2](1,0) =  f*s*tr;
+  second[0](1,1) = second[2](0,0) = -(second[1](1,0) = -f*tr*tr);
+  pluseq_scaled_I( second, 2 );
+  second_deriv_wrt_product_factor( second, Winv );
+  
+  return true;
+}
 
 } // namespace Mesquite
