af8e4fd3687f1824ed6de39f5e03c84cd33bc733
Jason Kraftcheck <kraftche@cae.wisc.edu>
2010-12-06 16:23:35 -0600

    make test of UntangleMu better

--- testSuite/unit/UntangleTargetMetricTests.cpp
+++ testSuite/unit/UntangleTargetMetricTests.cpp
@@ -4,13 +4,25 @@
 using namespace Mesquite;
 
 #include "TSizeNB1.hpp"
-#include "TShapeSizeNB3.hpp"
+#include "TShapeSize2DNB1.hpp"
+#include "TShapeSize3DNB1.hpp"
+#include "TMixed.hpp"
+#include "TScale.hpp"
 #include "TUntangleBeta.hpp"
 #include "TUntangle1.hpp"
 #include "TUntangleMu.hpp"
+
+class TUntangleShSz : public TMixed
+{
+public:
+  TShapeSize2DNB1 SS2D;
+  TShapeSize3DNB1 SS3D;
+  TScale SS2DS; // scale 2D value so that it is sensitive to shape deformation
+  TUntangleShSz() : TMixed(&SS2DS,&SS3D), SS2DS(10,&SS2D) {}
+};
 
 //                               NAME                !SHAPE !SIZE !ORIENT BARRIER
 TEST_METRIC_WITH_HESS   ( TUntangleBeta,              true,  true,  true, false );
 TEST_METRIC_WITH_HESS   ( TUntangle1,                 true,  true,  true, false );
 TEST_COMPOSITE_WITH_HESS( TUntangleMu, TSizeNB1,      true,  false, true, false );
-TEST_COMPOSITE_WITH_HESS( TUntangleMu, TShapeSizeNB3, true,  false, true, false );
+TEST_COMPOSITE_WITH_HESS( TUntangleMu, TUntangleShSz, false, false, true, false );
