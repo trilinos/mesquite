823cd48c0a1fb7703e0512a8428d32f12ddd93b8
Jason Kraftcheck <kraftche@cae.wisc.edu>
2010-12-06 13:09:04 -0600

    fix test suite build errors after TMP reorg

--- testSuite/unit/UntangleTargetMetricTests.cpp
+++ testSuite/unit/UntangleTargetMetricTests.cpp
@@ -4,13 +4,13 @@
 using namespace Mesquite;
 
 #include "TSizeNB1.hpp"
-#include "TShapeSizeNB1.hpp"
+#include "TShapeSizeNB3.hpp"
 #include "TUntangleBeta.hpp"
-#include "TUntangleAlt1.hpp"
+#include "TUntangle1.hpp"
 #include "TUntangleMu.hpp"
 
 //                               NAME                !SHAPE !SIZE !ORIENT BARRIER
 TEST_METRIC_WITH_HESS   ( TUntangleBeta,              true,  true,  true, false );
-TEST_METRIC_WITH_HESS   ( TUntangleAlt1,              true,  true,  true, false );
+TEST_METRIC_WITH_HESS   ( TUntangle1,                 true,  true,  true, false );
 TEST_COMPOSITE_WITH_HESS( TUntangleMu, TSizeNB1,      true,  false, true, false );
-TEST_COMPOSITE_WITH_HESS( TUntangleMu, TShapeSizeNB1, true,  false, true, false );
+TEST_COMPOSITE_WITH_HESS( TUntangleMu, TShapeSizeNB3, true,  false, true, false );
