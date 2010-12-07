5d6efa3226f3624bdf813f1d9e6bdf911f9307cf
Jason Kraftcheck <kraftche@cae.wisc.edu>
2010-12-07 11:25:59 -0600

    Move TSquared from ShapeSize/ to Misc/
    fix typo in CMakeLists.txt

--- testSuite/unit/MiscTargetMetricTests.cpp
+++ testSuite/unit/MiscTargetMetricTests.cpp
@@ -7,9 +7,36 @@ using namespace Mesquite;
 #include "AWSizeB1.hpp"
 #include "TSizeNB1.hpp"
 #include "TSizeB1.hpp"
+#include "TSquared.hpp"
 
 //                     NAME       !SHAPE !SIZE !ORIENT BARRIER
 TEST_METRIC_WITH_HESS( AWSizeNB1,  true, false,  true, false );
 TEST_METRIC_WITH_HESS( AWSizeB1,   true, false,  true,  true );
 TEST_METRIC_WITH_HESS( TSizeNB1,  true, false,  true, false );
 TEST_METRIC_WITH_GRAD( TSizeB1,   true, false,  true,  true );
+
+TSquared test_TSquared;
+class TSquared2DTest : public TMetricTest<TSquared,2> {
+  public: 
+    TSquared2DTest() : TMetricTest<TSquared,2>(false,false,false,false) {}
+    CPPUNIT_TEST_SUITE( TSquared2DTest );
+    CPPUNIT_TEST( compare_anaytic_and_numeric_grads );
+    CPPUNIT_TEST( compare_eval_with_grad_and_eval_with_hess );
+    CPPUNIT_TEST( compare_anaytic_and_numeric_hess );
+    CPPUNIT_TEST_SUITE_END();
+};
+class TSquared3DTest : public TMetricTest<TSquared,3> {
+  public: 
+    TSquared3DTest() : TMetricTest<TSquared,3>(false,false,false,false) {}
+    CPPUNIT_TEST_SUITE( TSquared3DTest );
+    CPPUNIT_TEST( compare_anaytic_and_numeric_grads );
+    CPPUNIT_TEST( compare_eval_with_grad_and_eval_with_hess );
+    CPPUNIT_TEST( compare_anaytic_and_numeric_hess );
+    CPPUNIT_TEST_SUITE_END();
+};
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TSquared2DTest, "Unit" );
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TSquared3DTest, "Unit" );
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TSquared2DTest, TARGET_TEST_GROUP );
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TSquared3DTest, TARGET_TEST_GROUP );
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TSquared2DTest, "TSquaredTest" );
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TSquared3DTest, "TSquaredTest" );
--- testSuite/unit/ShapeSizeTargetMetricTests.cpp
+++ testSuite/unit/ShapeSizeTargetMetricTests.cpp
@@ -13,7 +13,6 @@ using namespace Mesquite;
 #include "TShapeSizeB1.hpp"
 #include "TShapeSizeB3.hpp"
 #include "TShapeSizeNB3.hpp"
-#include "TSquared.hpp"
 
 //                            NAME     !SHAPE !SIZE !ORIENT BARRIER
 TEST_METRIC_WITH_GRAD   ( AWShapeSizeB1,  false,false, true,true  );
@@ -27,29 +26,3 @@ TEST_METRIC_WITH_HESS   ( TShapeSizeB1,   false,false, true,true  );
 TEST_METRIC_WITH_HESS   ( TShapeSizeB3,   false,false, true,true  );
 TEST_METRIC_WITH_HESS   ( TShapeSizeNB3,  false,false, true,false );
 
-
-TSquared test_TSquared;
-class TSquared2DTest : public TMetricTest<TSquared,2> {
-  public: 
-    TSquared2DTest() : TMetricTest<TSquared,2>(false,false,false,false) {}
-    CPPUNIT_TEST_SUITE( TSquared2DTest );
-    CPPUNIT_TEST( compare_anaytic_and_numeric_grads );
-    CPPUNIT_TEST( compare_eval_with_grad_and_eval_with_hess );
-    CPPUNIT_TEST( compare_anaytic_and_numeric_hess );
-    CPPUNIT_TEST_SUITE_END();
-};
-class TSquared3DTest : public TMetricTest<TSquared,3> {
-  public: 
-    TSquared3DTest() : TMetricTest<TSquared,3>(false,false,false,false) {}
-    CPPUNIT_TEST_SUITE( TSquared3DTest );
-    CPPUNIT_TEST( compare_anaytic_and_numeric_grads );
-    CPPUNIT_TEST( compare_eval_with_grad_and_eval_with_hess );
-    CPPUNIT_TEST( compare_anaytic_and_numeric_hess );
-    CPPUNIT_TEST_SUITE_END();
-};
-CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TSquared2DTest, "Unit" );
-CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TSquared3DTest, "Unit" );
-CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TSquared2DTest, TARGET_TEST_GROUP );
-CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TSquared3DTest, TARGET_TEST_GROUP );
-CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TSquared2DTest, "TSquaredTest" );
-CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TSquared3DTest, "TSquaredTest" );
