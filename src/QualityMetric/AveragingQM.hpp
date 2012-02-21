23d91a0387d6e91741c703bcbf4ab3afd1dee0fc
Stephen Kennon <srkenno@sandia.gov>
2012-02-21 10:23:39 -0700

    mesquite,percept: fix intel compile warnings
    (From Sierra)

--- src/QualityMetric/AveragingQM.hpp
+++ src/QualityMetric/AveragingQM.hpp
@@ -59,6 +59,8 @@ namespace MESQUITE_NS
      MESQUITE_EXPORT AveragingQM( QualityMetric::AveragingMethod method ) :
        avgMethod(method)
      {}
+
+     MESQUITE_EXPORT virtual ~AveragingQM() {}
      
        /*!Set the averaging method for the quality metric. */
      MESQUITE_EXPORT inline 
