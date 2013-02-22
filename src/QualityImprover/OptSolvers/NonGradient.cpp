1a9f1ddf1a95bdeb7424f92fc97705e381008a2b
bktidwe <boyd@elemtech.com>
2013-02-22 10:03:01 -0700

    Minor fix for NonGradient to allow absolute vertex movement termination to work correctly, added NonGradient to users guide, added NonGradient test to testSuite.

--- src/QualityImprover/OptSolvers/NonGradient.cpp
+++ src/QualityImprover/OptSolvers/NonGradient.cpp
@@ -185,6 +185,7 @@ NonGradient::evaluate( double *point,  PatchData &pd, MsqError &err )
 
   double value;
   bool feasible = obj_func.evaluate( pd, value, err ); MSQ_ERRZERO(err);
+  term_crit->accumulate_patch( pd, err ); //MSQ_ERRRTN(err);
   term_crit->accumulate_inner( pd, value, 0, err );  //MSQ_CHKERR(err);
   pd.set_vertex_coordinates( originalVec, vertexIndex, err ); 
   if( !feasible && mUseExactPenaltyFunction )  
---
+++ testSuite/nongradient_test/main.cpp
@@ -0,0 +1,195 @@
+/* ***************************************************************** 
+    MESQUITE -- The Mesh Quality Improvement Toolkit
+
+    Copyright 2004 Sandia Corporation and Argonne National
+    Laboratory.  Under the terms of Contract DE-AC04-94AL85000 
+    with Sandia Corporation, the U.S. Government retains certain 
+    rights in this software.
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License 
+    (lgpl.txt) along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+    diachin2@llnl.gov, djmelan@sandia.gov, mbrewer@sandia.gov, 
+    pknupp@sandia.gov, tleurent@mcs.anl.gov, tmunson@mcs.anl.gov      
+   
+  ***************************************************************** */
+
+/** \file main.cpp 
+ * \brief test NonGradient Solver (barrier and non-barrier).
+ * \author Boyd Tidwell
+ */
+#include "meshfiles.h"
+#include "Mesquite.hpp"
+#include "MeshImpl.hpp"
+#include "MsqError.hpp"
+#include "InstructionQueue.hpp"
+#include "TerminationCriterion.hpp"
+#include "QualityAssessor.hpp"
+#include "PlanarDomain.hpp"
+#include "XYPlanarDomain.hpp"
+#include "MeshInterface.hpp"
+
+#include "TShapeNB1.hpp"   
+#include "TShapeB1.hpp"
+#include "TQualityMetric.hpp"    
+#include "IdealShapeTarget.hpp"
+#include "MaxTemplate.hpp"
+#include "ElementMaxQM.hpp"
+#include "TargetCalculator.hpp"   
+#include "PMeanPTemplate.hpp"
+#include "LPtoPTemplate.hpp"
+#include "ElementPMeanP.hpp"
+#include "NonGradient.hpp"   
+
+using namespace Mesquite;
+
+int main()
+{
+  MsqPrintError err(std::cout);
+  PlanarDomain xyPlane(PlanarDomain::XY, -5);
+
+  #define FILE_NAME "bad_circle_tri.vtk"
+  const char file_name[] = MESH_FILES_DIR "2D/vtk/tris/untangled/" FILE_NAME;
+
+    // Barrier / Max Objective Function Test
+
+  Mesquite::MeshImpl mesh_max;
+  mesh_max.read_vtk(file_name, err);
+  if (err)
+  {
+    std::cerr << "NonGradient Barrier test: failed to read file." << std::endl;
+    return 1;
+  }
+
+  IdealShapeTarget target_max;
+  TShapeB1 mu;
+  TQualityMetric tqMetric_max( &target_max, &mu );
+
+  ElementQM* metricPtr_max;
+  ObjectiveFunctionTemplate* objFunctionPtr_max;
+
+  ElementMaxQM maxMetric( &tqMetric_max );
+  MaxTemplate maxObjFunction(&maxMetric);  // max(max)
+
+  LPtoPTemplate PtoPObjMaxfunction(&maxMetric, 1.0, err);  // max(max)
+
+  // Processing for Max Objective Function
+
+  NonGradient max_opt( &maxObjFunction ); // optimization procedure 
+
+  max_opt.setSimplexDiameterScale(0); 
+  max_opt.use_element_on_vertex_patch(); // local patch
+  max_opt.set_debugging_level(0);
+
+  // Construct and register the Quality Assessor instances
+  QualityAssessor max_initial_qa=QualityAssessor(&maxMetric, 10);
+  QualityAssessor maxObj_max_optimal_qa=QualityAssessor(&maxMetric, 10);
+   
+  //**************Set stopping criterion****************
+  TerminationCriterion innerTC, outerTC;
+
+  outerTC.add_iteration_limit(40);
+  innerTC.add_iteration_limit(20);
+  max_opt.set_outer_termination_criterion(&outerTC);
+  max_opt.set_inner_termination_criterion(&innerTC);
+
+  InstructionQueue queue1; 
+  queue1.add_quality_assessor(&max_initial_qa,err);
+  if (err) return 1;
+
+  queue1.set_master_quality_improver(&max_opt, err);   // max
+  if (err) return 1;
+
+  queue1.add_quality_assessor(&maxObj_max_optimal_qa,err);
+  if (err) return 1;
+
+  const char* outputFile;
+  outputFile = "initialMeshMaxBarrier.vtk";
+  mesh_max.write_vtk(outputFile, err); 
+  if (err)
+    std::cerr << outputFile << " : NonGradient Barrier test: failed to write file." << std::endl;
+ 
+  Mesquite::MeshDomainAssoc mesh_and_domain = MeshDomainAssoc(&mesh_max, &xyPlane);
+  queue1.run_instructions(&mesh_and_domain, err);
+  if (err) return 1;
+
+  outputFile = "optimizedMeshMaxBarrier.vtk";  
+  mesh_max.write_vtk(outputFile, err); 
+  if (err)
+    std::cerr << outputFile << " : NonGradient Barrier test: failed to write file." << std::endl;
+
+
+    // Non-Barrier / Ave Objective Function Test
+
+  Mesquite::MeshImpl mesh_mean;
+  mesh_mean.read_vtk(file_name, err);
+  if (err)
+  {
+    std::cerr << "NonGradient Non-barrier test: failed to read file." << std::endl;
+    return 1;
+  }
+
+  TShapeNB1 nonBarrier;
+  TargetCalculator* target;
+  IdealShapeTarget ident_target; 
+
+  target = &ident_target;
+
+  TQualityMetric tqMetric( target, &nonBarrier );
+ 
+  ElementPMeanP mean_metric( 1.0, &tqMetric );
+  PMeanPTemplate meanObjfunction( 1.0, &mean_metric );  // ave(ave)
+ 
+  // Processing for Mean Objective Function
+  NonGradient mean_opt( &meanObjfunction ); // optimization procedure 
+  mean_opt.setExactPenaltyFunction(false); // allow infeasible
+  mean_opt.use_element_on_vertex_patch(); // local patch
+
+    // Set Termination Criteria
+  TerminationCriterion innerTC_mean, outerTC_mean;
+  outerTC_mean.add_iteration_limit(10);
+  innerTC_mean.add_iteration_limit(30);
+  mean_opt.set_outer_termination_criterion(&outerTC_mean);
+  mean_opt.set_inner_termination_criterion(&innerTC_mean);
+  mean_opt.set_debugging_level(0);
+
+  // Construct and register the meanObj Quality Assessor instance
+  QualityAssessor mean_qa=QualityAssessor(&mean_metric, 10);
+
+  InstructionQueue queue2; 
+  queue2.add_quality_assessor(&mean_qa, err);
+  if (err) return 1;
+  queue2.set_master_quality_improver(&mean_opt, err); 
+  if (err) return 1;
+  queue2.add_quality_assessor(&mean_qa, err);
+  if (err) return 1;
+
+  const char* outputFile_mean;
+  outputFile_mean = "initialMeshMeanNB.vtk";
+  mesh_mean.write_vtk(outputFile_mean, err); 
+
+  if (err)
+    std::cerr << outputFile_mean << " : NonGradient Non-barrier test: failed to write file." << std::endl;
+  
+  queue2.run_instructions(&mesh_and_domain, err);
+  if (err) return 1;
+  
+   outputFile = "optimizedMeshMeanNB.vtk";  
+   mesh_mean.write_vtk(outputFile_mean, err); 
+
+  if (err)
+    std::cerr << outputFile_mean << " : NonGradient Non-barrier test: failed to write file." << std::endl;
+   
+  return 0;
+}
